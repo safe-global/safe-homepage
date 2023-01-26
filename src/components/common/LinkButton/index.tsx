87e1f4db505cb8baca3d21ad40fada266caaec57
Usame Algan <usame@safe.global>
2023-01-26 13:57:58 +0100

    add arrow animation, adjust card block hover area

--- src/components/common/LinkButton/index.tsx
+++ src/components/common/LinkButton/index.tsx
@@ -1,10 +1,14 @@
 import { ButtonBase, ButtonProps } from '@mui/material'
 import css from './styles.module.css'
+import AngleIcon from '@/public/images/angle-icon.svg'
 
 const LinkButton = ({ children, ...props }: ButtonProps) => {
   return (
     <ButtonBase className={css.linkButton} {...props}>
       {children}
+      <div className={css.arrow}>
+        <AngleIcon />
+      </div>
     </ButtonBase>
   )
 }
