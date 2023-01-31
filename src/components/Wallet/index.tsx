67ba33b8ba87b72112a426e9ed05842d255fd70e
Diogo Soares <diogo.lsoares@gmail.com>
2023-01-31 09:41:59 +0100

    move content file to parent dir

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,10 +1,9 @@
 import UspBlock from '@/components/Wallet/UspBlock'
-import { YourKeysContent } from '@/components/Wallet/UspBlock/content'
+import { YourKeysContent } from '@/components/Wallet/content'
 
-export const Wallet = () => {
-  return (
-    <>
-      <UspBlock content={YourKeysContent} />
-    </>
-  )
-}
+export const Wallet = () => (
+  <>
+    <UspBlock content={YourKeysContent} />
+  </>
+)
+
