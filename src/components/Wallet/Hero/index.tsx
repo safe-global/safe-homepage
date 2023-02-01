4df90cedf857ec000726ed17b52a8c9309515907
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-02-01 17:35:12 +0100

    Section hero (#18)
    * create HeroSection
    
    * amend title styles
    
    * fix buttons alignment

---
+++ src/components/Wallet/Hero/index.tsx
@@ -0,0 +1,66 @@
+import { ReactElement } from 'react'
+import Image from 'next/image'
+import { Button, Container, Grid, Typography } from '@mui/material'
+import { IOS_LINK, GPLAY_LINK } from '@/config/constants'
+import IOSDownload from '@/public/images/ios-download.svg'
+import GPlayDownload from '@/public/images/google-play-download.svg'
+import QRImage from '@/public/images/Wallet/wallet-QR.png'
+
+import css from './styles.module.css'
+
+export type HeroSectionProps = {
+  image: JSX.Element
+  textBlock: {
+    title: string
+    button: {
+      text: string
+      href: string
+    }
+  }
+}
+
+const HeroSection = ({ image, textBlock }: HeroSectionProps): ReactElement => {
+  const { title, button } = textBlock
+  const { text: buttonText, href } = button
+
+  return (
+    <Container>
+      <Grid container className={css.container} spacing={{ xs: 6, md: '30px' }} justifyContent="space-between">
+        <Grid item md={6}>
+          {image}
+        </Grid>
+        <Grid item md={6}>
+          <Typography className={css.title} variant="h1" mb={5}>
+            {title}
+          </Typography>
+          <Grid container gap={4} flexDirection="column" justifyContent="flex-start">
+            <Grid item>
+              <Button key={buttonText} href={href} variant="contained" size="large">
+                {buttonText}
+              </Button>
+            </Grid>
+            <Grid item>
+              <Grid container gap="10px" className={css.links}>
+                <Grid item>
+                  <a href={IOS_LINK} target="_blank" rel="noreferrer" aria-label="AppStore download">
+                    <IOSDownload />
+                  </a>
+                </Grid>
+                <Grid item>
+                  <a href={GPLAY_LINK} target="_blank" rel="noreferrer" aria-label="Google Play download">
+                    <GPlayDownload />
+                  </a>
+                </Grid>
+                <Grid item>
+                  <Image src={QRImage} alt="Mobile wallet" />
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default HeroSection
--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -2,14 +2,17 @@ import UspBlock from '@/components/Wallet/UspBlock'
 import {
   BatchTransactionsContent,
   CoManageContent,
+  HeroContent,
   OwnershipContent,
   SignerTypesContent,
   YourKeysContent,
 } from '@/components/Wallet/content'
 import ImageText from '@/components/Wallet/ImageText'
+import HeroSection from '@/components/Wallet/Hero'
 
 export const Wallet = () => (
   <>
+    <HeroSection {...HeroContent} />
     <UspBlock {...YourKeysContent} />
     <ImageText {...CoManageContent} />
     <ImageText {...OwnershipContent} />
