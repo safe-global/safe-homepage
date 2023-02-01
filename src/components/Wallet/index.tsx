466a4ef16371ea00adfbbf63060eec5700f08ebf
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-02-01 16:28:48 +0100

    Section ImageText (#16)
    * add ImageText sections
    
    * make image come first in mobile
    
    * adjust dimensions as per designs
    
    * pr comments

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,9 +1,20 @@
 import UspBlock from '@/components/Wallet/UspBlock'
-import { YourKeysContent } from '@/components/Wallet/content'
+import {
+  BatchTransactionsContent,
+  CoManageContent,
+  OwnershipContent,
+  SignerTypesContent,
+  YourKeysContent,
+} from '@/components/Wallet/content'
+import ImageText from '@/components/Wallet/ImageText'
 
 export const Wallet = () => (
   <>
     <UspBlock {...YourKeysContent} />
+    <ImageText {...CoManageContent} />
+    <ImageText {...OwnershipContent} />
+    <ImageText {...BatchTransactionsContent} />
+    <ImageText {...SignerTypesContent} />
   </>
 )
 
