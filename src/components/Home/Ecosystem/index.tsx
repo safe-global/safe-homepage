025feefc98cf32d88ae49c0bfd306d0a39fe362a
Usame Algan <usame@safe.global>
2023-02-02 15:51:49 +0100

    fix: Add parallax effect to ecosystem icons

--- src/components/Home/Ecosystem/index.tsx
+++ src/components/Home/Ecosystem/index.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useRef, type ReactElement } from 'react'
 import { Grid, Typography, Container, Box } from '@mui/material'
-import type { ReactElement } from 'react'
 
 import css from './styles.module.css'
 import type { BaseBlock } from '@/components/Home/types'
 
 const Ecosystem = ({ title, text }: BaseBlock): ReactElement => {
+  const bgRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (!bgRef.current) return
+
+    const parallax = () => {
+      let _h = window.innerHeight / 2
+      let _scrollPos = window.scrollY
+      let _depth1 = `center ${50 - (_scrollPos * 0.1 - _h) * 0.1}%`
+      let _depth2 = `center ${50 - (_scrollPos * 0.1 - _h) * 0.15}%`
+      let _depth3 = `center ${50 - (_scrollPos * 0.1 - _h) * 0.2}%`
+      let x = `${_depth3}, ${_depth2}, ${_depth1}`
+
+      if (bgRef.current) {
+        bgRef.current.style.backgroundPosition = x
+      }
+    }
+
+    document.addEventListener('scroll', parallax)
+
+    return () => {
+      document.removeEventListener('scroll', parallax)
+    }
+  }, [])
+
   return (
-    <div className={css.bg}>
+    <div className={css.bg} ref={bgRef}>
       <Container sx={{ textAlign: 'center' }}>
         <Box height={{ xs: '700px', md: '1090px' }} display="flex" alignItems="center">
           <Grid container justifyContent="center">
