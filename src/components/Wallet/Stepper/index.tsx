64c4b7450f96e2611869217e3b63f2769c93bf53
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-02-03 16:15:42 +0100

    Section convenience (#24)
    * add subBlock to ImageText. add Stepper component
    
    * improve styles
    
    * use clsx and create enum
    
    * Update src/components/Wallet/Stepper/index.tsx
    
    Co-authored-by: Aaron Cook <aaron@safe.global>
    
    ---------
    
    Co-authored-by: Aaron Cook <aaron@safe.global>

---
+++ src/components/Wallet/Stepper/index.tsx
@@ -0,0 +1,43 @@
+import { Step, StepContent, StepLabel, Stepper, Typography } from '@mui/material'
+import CircleIcon from '@/public/images/Wallet/circle.svg'
+import css from './styles.module.css'
+import clsx from 'clsx'
+
+export enum StepState {
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
+}
+
+export type StepsType = {
+  label: string
+  description?: string
+  state?: StepState
+}[]
+
+const CustomStepper = ({ steps }: { steps: StepsType }) => (
+  <Stepper
+    activeStep={steps.findIndex(({ state }) => state === StepState.ACTIVE) || 0}
+    orientation="vertical"
+    className={css.stepper}
+  >
+    {steps.map(({ label, description, state }) => (
+      <Step key={label}>
+        <StepLabel
+          sx={{ py: 0 }}
+          icon={<CircleIcon className={clsx(css.icon, state && css.iconActive)} />}
+        >
+          <Typography variant="h4" color="white">
+            {label}
+          </Typography>
+        </StepLabel>
+        <StepContent className={clsx(css.stepContent, state === StepState.ACTIVE && css.activeBorder)}>
+          <Typography variant="body1" color="primary.light" marginTop="18px">
+            {description}
+          </Typography>
+        </StepContent>
+      </Step>
+    ))}
+  </Stepper>
+)
+
+export default CustomStepper
