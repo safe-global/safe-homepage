2ecb046c6318f2c6ea3478aa11b3729a5cdd6132
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-02-09 13:15:11 +0100

    feat: safe core protocol section (#46)
    * feat: AA Stack section
    
    * style the Card
    
    * rename component
    
    * feat: TextGrid
    
    * refactor: use object with src alt props in content
    
    * fix text

---
+++ src/components/Core/TextGrid/index.tsx
@@ -0,0 +1,75 @@
+import { Chip, Container, Grid, Typography } from '@mui/material'
+import layoutCss from '@/components/common/styles.module.css'
+import LinkButton from '@/components/common/LinkButton'
+import css from './styles.module.css'
+
+export type TextGridProps = {
+  image: {
+    src: string
+    alt: string
+  }
+  textBlock: {
+    caption: string
+    title: JSX.Element
+    text: string
+    link: {
+      text: string
+      href: string
+    }
+  }
+  grid: {
+    title: string
+    items: {
+      text: string
+    }[]
+  }
+}
+
+const TextGrid = ({ image, textBlock, grid }: TextGridProps) => {
+  const { caption, title, text, link } = textBlock
+  const { title: gridTitle, items } = grid
+
+  return (
+    <Container>
+      <Grid container className={layoutCss.containerShort}>
+        <Grid container className={css.wrapper} justifyContent="space-between">
+          <Grid item md={5} display="flex" flexDirection="column" justifyContent="center" gap={{ xs: 3, md: 4 }}>
+            <Chip
+              label={
+                <Typography variant="caption" color="primary.main">
+                  {caption}
+                </Typography>
+              }
+              className={css.chip}
+              variant="outlined"
+            />
+            <Typography variant="h2">{title}</Typography>
+            <Typography>{text}</Typography>
+            <LinkButton href={link.href} sx={{ width: 'fit-content' }}>
+              {link.text}
+            </LinkButton>
+          </Grid>
+          <Grid item md={6}>
+            <img src={image.src} alt={image.alt} />
+          </Grid>
+        </Grid>
+        <Grid container mt={{ xs: 5, md: 10 }}>
+          <Typography variant="caption" component="div" mb={3}>
+            {gridTitle}
+          </Typography>
+          <Grid container>
+            {items.map(({ text }, index) => (
+              <Grid key={index} item xs={12} md={12 / items.length} className={css.card}>
+                <Typography variant="h4" color="text.primary">
+                  {text}
+                </Typography>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default TextGrid
