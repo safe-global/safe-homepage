ee6d9bb1592225a0a6b5286a0a0288713e1678c9
Diogo Soares <diogo.lsoares@gmail.com>
2023-02-10 15:19:06 +0100

    refactor: use getStaticProps to fetch chains

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,4 +1,8 @@
 import { walletContent } from '@/components/Wallet/content'
+import type { getStaticProps } from '@/pages/wallet'
+import type { InferGetStaticPropsType } from 'next'
 import PageContent from '../common/PageContent'
 
-export const Wallet = () => <PageContent content={walletContent} />
+export const Wallet = (props: InferGetStaticPropsType<typeof getStaticProps>) => (
+  <PageContent content={walletContent} {...props} />
+)
--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,17 +1,41 @@
 import Head from 'next/head'
-import type { NextPage } from 'next'
-
+import type { InferGetStaticPropsType, NextPage } from 'next'
 import { Wallet } from '@/components/Wallet'
+import { CHAINS_URL } from '@/config/constants'
+import type { ChainProps } from '@/components/common/Networks'
 
-const WalletPage: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Safe {`{Wallet}`}</title>
-      </Head>
-      <Wallet />
-    </>
-  )
+const WalletPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (props) => (
+  <>
+    <Head>
+      <title>Safe {`{Wallet}`}</title>
+    </Head>
+    <Wallet {...props} />
+  </>
+)
+
+export async function getStaticProps() {
+  const chainsData: ChainProps[] = await fetch(CHAINS_URL)
+    .then((res) => res.json())
+    .then(({ results }) => {
+      return results.map(
+        (
+          chain: {
+            chainName: string
+            theme: { textColor: string; backgroundColor: string }
+          } & Record<string, unknown>,
+        ) => ({
+          chainName: chain.chainName,
+          textColor: chain.theme.textColor,
+          backgroundColor: chain.theme.backgroundColor,
+        }),
+      )
+    })
+
+  return {
+    props: {
+      chainsData,
+    },
+  }
 }
 
 export default WalletPage
