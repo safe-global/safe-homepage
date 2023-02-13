417d9def99e5fa322e83704a7c43c5ddf756c288
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-02-13 11:17:16 +0100

    refactor: use getStaticProps to fetch chains (#57)
    * refactor: use getStaticProps to fetch chains
    
    * extract loadChainsData
    
    * add fetch chains server-side to the Home page
    
    * add dependency of gw sdk

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,7 +1,9 @@
 import type { ReactElement } from 'react'
 import { homeContent } from '@/components/Home/content'
 import PageContent from '../common/PageContent'
+import type { InferGetStaticPropsType } from 'next'
+import type { getStaticProps } from '@/pages'
 
-export const Home = (): ReactElement => {
-  return <PageContent content={homeContent} />
+export const Home = (props: InferGetStaticPropsType<typeof getStaticProps>): ReactElement => {
+  return <PageContent content={homeContent} {...props} />
 }
