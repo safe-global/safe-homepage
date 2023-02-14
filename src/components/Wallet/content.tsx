20ee37309cedf3dbfeb0ada01179aab4cb3f3110
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-02-14 11:52:39 +0100

    feat: generic ParallaxText (#63)
    * feat: generic ParallaxText
    
    * move layer positions to content. extract Parallax jsx
    
    * feat: app store parallax

--- src/components/Wallet/content.tsx
+++ src/components/Wallet/content.tsx
@@ -78,16 +78,48 @@ export const walletContent = [
   },
   {
     variant: 'text-image',
-    image: {
-      src: '/images/Wallet/safe-apps-store.png',
-      alt: 'Transactions require confirmations from owners',
+    parallax: {
+      baseImage: {
+        src: '/images/Wallet/Parallaxes/AppStore/background.svg',
+        alt: 'Parallax base image',
+      },
+      layers: [
+        {
+          image: {
+            src: '/images/Wallet/Parallaxes/AppStore/logos1.svg',
+            alt: 'Address 1',
+          },
+          translateX: 0,
+          translateY: 0,
+          depth: 2,
+          direction: -1,
+          initialPosition: {
+            top: '-400px',
+            left: '0',
+          },
+        },
+        {
+          image: {
+            src: '/images/Wallet/Parallaxes/AppStore/logos2.svg',
+            alt: 'Address 2',
+          },
+          translateX: 0,
+          translateY: 0,
+          depth: 0,
+          direction: -1,
+          initialPosition: {
+            bottom: '50px',
+            right: '0',
+          },
+        },
+      ],
     },
     textBlock: {
       title: 'Built-in app store',
       text: 'Use the best dapps in web3 right from inside your Safe WALLET.',
       buttons: [{ text: 'Get started', href: '#', variant: 'link' }],
     },
-    component: ImageText,
+    component: ParallaxText,
   },
   {
     variant: 'image-text',
