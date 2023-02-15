e92e5238d6c8090901dcb8c1ca800d95c398b0e0
Usame Algan <usame@safe.global>
2023-02-15 16:45:03 +0100

    improve FF performance

--- src/components/Careers/FloatingTiles/index.tsx
+++ src/components/Careers/FloatingTiles/index.tsx
@@ -112,7 +112,7 @@ export const FloatingTiles = () => (
     <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
       <defs>
         <filter id="gooey">
-          <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
+          <feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" colorInterpolationFilters="sRGB" />
           <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
           <feComposite in="SourceGraphic" in2="goo" operator="atop" />
         </filter>
