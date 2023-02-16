f5cf4e3222c05b3d5dacc6be8f85830e57c289b7
Diogo Soares <diogo.lsoares@gmail.com>
2023-02-16 14:31:18 +0100

    use pngs for the parallax layers in signer wallets section

--- src/components/Wallet/ParallaxSignerWallets/ParallaxSignerWalletsElement.tsx
+++ src/components/Wallet/ParallaxSignerWallets/ParallaxSignerWalletsElement.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 import FrameImage from '@/public/images/Wallet/Parallaxes/SignerWallets/background.svg'
 import PhoneImage from '@/public/images/Wallet/Parallaxes/SignerWallets/phone.png'
-import Wallets1Image from '@/public/images/Wallet/Parallaxes/SignerWallets/wallets1.svg'
-import Wallets2Image from '@/public/images/Wallet/Parallaxes/SignerWallets/wallets2.svg'
+import Wallets1Image from '@/public/images/Wallet/Parallaxes/SignerWallets/wallets1.png'
+import Wallets2Image from '@/public/images/Wallet/Parallaxes/SignerWallets/wallets2.png'
 import ParallaxWrapper from '@/components/common/ParallaxWrapper'
 import css from './styles.module.css'
 
@@ -11,13 +11,13 @@ const ParallaxBatchingElement = () => {
     <div className={css.parallaxWrapper}>
       <FrameImage className={css.baseImage} />
       <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <Wallets2Image className={css.wallets2} />
+        <Image src={Wallets2Image} alt="Wallets logos" className={css.wallets2} />
       </ParallaxWrapper>
       <ParallaxWrapper translateX={0} translateY={0} depth={3} direction={-1}>
         <Image src={PhoneImage} alt="Assets view on Safe mobile" className={css.phone} />
       </ParallaxWrapper>
       <ParallaxWrapper translateX={0} translateY={0} depth={1} direction={-1}>
-        <Wallets1Image className={css.wallets1} />
+        <Image src={Wallets1Image} alt="Wallets logos" className={css.wallets1} />
       </ParallaxWrapper>
     </div>
   )
