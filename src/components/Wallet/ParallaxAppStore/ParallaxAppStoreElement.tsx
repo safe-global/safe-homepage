c8e0dc357dd42c14119b4cab2d499435f733a8e7
Diogo Soares <diogo.lsoares@gmail.com>
2023-02-16 14:02:42 +0100

    use pngs for the parallax layers in ap store section

--- src/components/Wallet/ParallaxAppStore/ParallaxAppStoreElement.tsx
+++ src/components/Wallet/ParallaxAppStore/ParallaxAppStoreElement.tsx
@@ -1,5 +1,6 @@
-import Logos1Image from '@/public/images/Wallet/Parallaxes/AppStore/logos1.svg'
-import Logos2Image from '@/public/images/Wallet/Parallaxes/AppStore/logos2.svg'
+import Image from 'next/image'
+import Logos1Image from '@/public/images/Wallet/Parallaxes/AppStore/logos1.png'
+import Logos2Image from '@/public/images/Wallet/Parallaxes/AppStore/logos2.png'
 import FrameImage from '@/public/images/Wallet/Parallaxes/AppStore/background.svg'
 import ParallaxWrapper from '@/components/common/ParallaxWrapper'
 import css from './styles.module.css'
@@ -9,10 +10,10 @@ const ParallaxAppStoreElement = () => {
     <div className={css.parallaxWrapper}>
       <FrameImage className={css.baseImage} />
       <ParallaxWrapper translateX={0} translateY={0} depth={2} direction={-1}>
-        <Logos1Image className={css.logos1} />
+        <Image src={Logos1Image} alt="Safe apps logos" className={css.logos1} />
       </ParallaxWrapper>
       <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <Logos2Image className={css.logos2} />
+        <Image src={Logos2Image} alt="Safe apps logos" className={css.logos2} />
       </ParallaxWrapper>
     </div>
   )
