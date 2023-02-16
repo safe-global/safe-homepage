017602c33e503def1edeb3f6c857f58a58fa6767
Usame Algan <usame@safe.global>
2023-02-16 09:39:00 +0100

    fix: Use png instead of svg for phone

--- src/components/Wallet/MobileAppParallax/MobileAppParallaxElement.tsx
+++ src/components/Wallet/MobileAppParallax/MobileAppParallaxElement.tsx
@@ -1,14 +1,15 @@
-import PhoneImage from '@/public/images/Wallet/Parallaxes/Mobile/phone.svg'
+import PhoneImage from '@/public/images/Wallet/Parallaxes/Mobile/phone.png'
 import FrameImage from '@/public/images/Wallet/Parallaxes/Mobile/background.svg'
 import ParallaxWrapper from '@/components/common/ParallaxWrapper'
 import css from './styles.module.css'
+import Image from 'next/image'
 
 const MobileAppParallaxElement = () => {
   return (
     <div className={css.parallaxWrapper}>
       <FrameImage className={css.baseImage} />
       <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <PhoneImage className={css.phone} />
+        <Image className={css.phone} src={PhoneImage} alt="A phone showing the mobile app" />
       </ParallaxWrapper>
     </div>
   )
