c11e4ee539e56d801fa521e5e8886dfa27599755
Diogo Soares <diogo.lsoares@gmail.com>
2023-02-16 14:43:30 +0100

    use pngs for the parallax layers in our locations section

--- src/components/Careers/ParallaxLocations/ParallaxLocationsElement.tsx
+++ src/components/Careers/ParallaxLocations/ParallaxLocationsElement.tsx
@@ -1,8 +1,6 @@
+import Image from 'next/image'
 import FrameImage from '@/public/images/Careers/Locations/background.svg'
-import Location1Image from '@/public/images/Careers/Locations/location1.svg'
-import Location2Image from '@/public/images/Careers/Locations/location2.svg'
-import Location3Image from '@/public/images/Careers/Locations/location3.svg'
-import Location4Image from '@/public/images/Careers/Locations/location4.svg'
+import LocationsImage from '@/public/images/Careers/Locations/locations.png'
 import ParallaxWrapper from '@/components/common/ParallaxWrapper'
 import css from './styles.module.css'
 
@@ -11,16 +9,7 @@ const ParallaxLocationsElement = () => {
     <div className={css.parallaxWrapper}>
       <FrameImage className={css.baseImage} />
       <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <Location1Image className={css.location1} />
-      </ParallaxWrapper>
-      <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <Location2Image className={css.location2} />
-      </ParallaxWrapper>
-      <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <Location3Image className={css.location3} />
-      </ParallaxWrapper>
-      <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <Location4Image className={css.location4} />
+        <Image src={LocationsImage} alt="Safe hub locations" className={css.locations} />
       </ParallaxWrapper>
     </div>
   )
