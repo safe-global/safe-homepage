f22ce0e4f0bc3bd493d5263c3d82d5d2e4ac148f
katspaugh <katspaugh@gmail.com>
2023-02-20 15:08:02 +0100

    Fix: hydration issues with require and swr

--- src/pages/careers.tsx
+++ src/pages/careers.tsx
@@ -1,17 +1,31 @@
 import Head from 'next/head'
-import type { NextPage } from 'next'
-
+import type { InferGetStaticPropsType, NextPage } from 'next'
+import { SWRConfig } from 'swr'
 import { Careers } from '@/components/Careers'
+import { fetchOpenPositions, SWR_KEY } from '@/hooks/useOpenPositions'
 
-const CareersPage: NextPage = () => {
+const CareersPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (props) => {
   return (
     <>
       <Head>
         <title>Safe – Careers</title>
       </Head>
-      <Careers />
+
+      <SWRConfig value={{ fallback: props.fallback }}>
+        <Careers />
+      </SWRConfig>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      fallback: {
+        [SWR_KEY]: await fetchOpenPositions(),
+      },
+    },
+  }
 }
 
 export default CareersPage
