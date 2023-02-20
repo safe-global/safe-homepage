d722984fad9888d7e5634b000a7a2ca2d479c8e1
katspaugh <katspaugh@gmail.com>
2023-02-20 11:33:47 +0100

    Core page

--- src/components/Core/index.tsx
+++ src/components/Core/index.tsx
@@ -1,6 +1,6 @@
 import type { ReactElement } from 'react'
 import PageContent from '@/components/common/PageContent'
-import { coreContent } from '@/components/Core/content'
+import coreContent from '@/content/core.json'
 
 export const Core = (): ReactElement => {
   return <PageContent content={coreContent} />
