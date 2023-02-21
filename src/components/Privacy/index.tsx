4277b6fbdb67e0ad3598afac110fcd8928f89c7d
katspaugh <katspaugh@gmail.com>
2023-02-21 12:15:51 +0100

    Restore some styles

--- src/components/Privacy/index.tsx
+++ src/components/Privacy/index.tsx
@@ -2,4 +2,6 @@ import type { ReactElement } from 'react'
 import PageContent from '@/components/common/PageContent'
 import privacyContent from '@/content/privacy.json'
 
-export const Privacy = (): ReactElement => <PageContent content={privacyContent} />
+export const Privacy = (): ReactElement => {
+  return <PageContent content={privacyContent} />
+}
