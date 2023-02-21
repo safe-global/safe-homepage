83fe735de8ec91d626ad9d93373cae58f808fa58
Diogo Soares <diogo.lsoares@gmail.com>
2023-02-21 18:26:27 +0100

    feat: tweets section

---
+++ src/components/Core/Tweets/index.tsx
@@ -0,0 +1,42 @@
+import { Container, Typography } from '@mui/material'
+import css from './styles.module.css'
+
+type TweetsSectionProps = {
+  caption: string
+  title: string
+  items: TweetProps[]
+}
+
+type TweetProps = {
+  id: string
+  src: string
+  alt: string
+  href: string
+}
+
+const Tweets = ({ caption, title, items }: TweetsSectionProps) => {
+  return (
+    <>
+      <Container>
+        <Typography variant="caption" textAlign="center" component="div" mt="100px" mb={3}>
+          {caption}
+        </Typography>
+        <Typography variant="h2" textAlign="center" maxWidth="670px" marginX="auto" mb="100px">
+          {title}
+        </Typography>
+      </Container>
+
+      <div className={css.gradient}>
+        <div className={css.tweetsGrid}>
+          {items.map(({ src, alt, href }, index) => (
+            <a key={index} href={href}>
+              <img src={src} alt={alt} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Tweets
