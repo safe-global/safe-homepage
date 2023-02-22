057bb1d5f3c987b0cd44ed2765cb08bee40cf2a0
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2023-02-22 11:41:58 +0100

    Add Favicon meta tags to document (#112)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ import { CacheProvider } from '@emotion/react'
 import type { EmotionCache } from '@emotion/react'
 import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
-import Head from 'next/head'
 
 import { createEmotionCache } from '@/styles/emotion'
 
@@ -15,7 +14,6 @@ import { theme } from '@/styles/theme'
 
 import '@/styles/globals.css'
 import PageLayout from '@/components/common/PageLayout'
-import MetaTags from '@/components/common/MetaTags'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -31,21 +29,15 @@ const App = ({
   emotionCache?: EmotionCache
 }): ReactElement => {
   return (
-    <>
-      <Head>
-        <title key="default-title">Safe</title>
-        <MetaTags />
-      </Head>
-      <CacheProvider value={emotionCache}>
-        <CssVarsProvider theme={cssVarsTheme}>
-          <CssBaseline />
+    <CacheProvider value={emotionCache}>
+      <CssVarsProvider theme={cssVarsTheme}>
+        <CssBaseline />
 
-          <PageLayout>
-            <Component {...pageProps} />
-          </PageLayout>
-        </CssVarsProvider>
-      </CacheProvider>
-    </>
+        <PageLayout>
+          <Component {...pageProps} />
+        </PageLayout>
+      </CssVarsProvider>
+    </CacheProvider>
   )
 }
 
