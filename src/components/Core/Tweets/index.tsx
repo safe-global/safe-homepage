15430c83425484f455220a952c1e296e7ae59f2e
Diogo Soares <diogo.lsoares@gmail.com>
2023-02-22 14:17:46 +0100

    add scroll-snap

--- src/components/Core/Tweets/index.tsx
+++ src/components/Core/Tweets/index.tsx
@@ -14,29 +14,27 @@ type TweetProps = {
   href: string
 }
 
-const Tweets = ({ caption, title, items }: TweetsSectionProps) => {
-  return (
-    <>
-      <Container>
-        <Typography variant="caption" textAlign="center" component="div" mt="100px" mb={3}>
-          {caption}
-        </Typography>
-        <Typography variant="h2" textAlign="center" maxWidth="670px" marginX="auto" mb="100px">
-          {title}
-        </Typography>
-      </Container>
+const Tweets = ({ caption, title, items }: TweetsSectionProps) => (
+  <>
+    <Container>
+      <Typography variant="caption" textAlign="center" component="div" mt="100px" mb={3}>
+        {caption}
+      </Typography>
+      <Typography variant="h2" textAlign="center" maxWidth="670px" marginX="auto" mb="100px">
+        {title}
+      </Typography>
+    </Container>
 
-      <div className={css.gradient}>
-        <div className={css.tweetsGrid}>
-          {items.map(({ src, alt, href }, index) => (
-            <Link key={index} href={href} target="_blank" rel="noopener">
-              <img src={src} alt={alt} loading="lazy" />
-            </Link>
-          ))}
-        </div>
+    <div className={css.gradient}>
+      <div className={css.tweetsGrid}>
+        {items.map(({ src, alt, href }, index) => (
+          <Link key={index} href={href} target="_blank" rel="noopener">
+            <img src={src} alt={alt} loading="lazy" />
+          </Link>
+        ))}
       </div>
-    </>
-  )
-}
+    </div>
+  </>
+)
 
 export default Tweets
