8f4b09972949974ce8eb0789245df7e483ab0e3d
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2023-02-23 10:16:01 +0100

    Use pseudo element braces for core and wallet types (#123)

--- src/components/common/SafeCoreText/index.tsx
+++
@@ -1,8 +0,0 @@
-import SafeCoreType from '@/public/images/safecore.svg'
-import type { TypographyProps } from '@mui/material'
-
-const SafeCoreText = ({ variant = 'body1' }: { variant: TypographyProps['variant'] }) => {
-  return <SafeCoreType className={`${variant}SafeType`} />
-}
-
-export default SafeCoreText
