54c04def0f8547e7ab092a449b49bedffe8d4bc1
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2023-02-23 14:04:51 +0100

    Add core cta banner, update careers banner accordingly (#116)
    * Add core cta banner, update careers banner accordingly
    
    * Adjust button colors, add core banner link

--- src/components/Careers/BannerCta/index.tsx
+++ src/components/Careers/BannerCta/index.tsx
@@ -4,26 +4,17 @@ import type { ReactElement } from 'react'
 
 import layoutCss from '@/components/common/styles.module.css'
 import css from './styles.module.css'
+import clsx from 'clsx'
+import type { BaseBlock } from '@/components/Home/types'
 
-export const BannerCta = ({
-  title,
-  text,
-  link,
-}: {
-  title: string
-  text: string
-  link: {
-    title: string
-    href: string
-  }
-}): ReactElement => {
+export const BannerCta = ({ title, text, link }: BaseBlock): ReactElement => {
   return (
-    <Container className={layoutCss.container}>
+    <Container className={clsx(layoutCss.containerShort, css.wrapper)}>
       <div className={css.container}>
         <Container>
           <Grid container>
             <Grid item xs={12} md={5}>
-              <Typography variant="h2" className={css.title}>
+              <Typography variant="h2" mb={{ xs: 3, md: 0 }}>
                 {title}
               </Typography>
             </Grid>
@@ -32,17 +23,18 @@ export const BannerCta = ({
               <Typography variant="h4" mb={2}>
                 {text}
               </Typography>
-              <Button
-                target="_top"
-                variant="contained"
-                size="large"
-                rel="noopener noreferrer"
-                href={link.href}
-                className={css.button}
-                color="background"
-              >
-                {link.title}
-              </Button>
+              {link && (
+                <Button
+                  target="_top"
+                  variant="contained"
+                  size="large"
+                  rel="noopener noreferrer"
+                  href={link.href}
+                  color="secondary"
+                >
+                  {link.title}
+                </Button>
+              )}
             </Grid>
           </Grid>
         </Container>
