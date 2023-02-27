d87a21aac3ca4978781b810f6e117426c9972495
Usame Algan <usame@safe.global>
2023-02-27 09:54:07 +0100

    Add mobile version of AA SDK video

--- src/components/Core/AASdk/index.tsx
+++ src/components/Core/AASdk/index.tsx
@@ -1,13 +1,16 @@
 import type { BaseBlock } from '@/components/Home/types'
-import { Chip, Container, Grid, Typography } from '@mui/material'
+import { Chip, Container, Grid, Typography, useMediaQuery } from '@mui/material'
 import layoutCss from '@/components/common/styles.module.css'
 import css from './styles.module.css'
 import LinkButton from '@/components/common/LinkButton'
 import clsx from 'clsx'
 import Link from 'next/link'
 import { RELAY_KIT_LINK, ONRAMP_KIT_LINK, AUTH_KIT_LINK, PROTOCOL_KIT_LINK } from '@/config/constants'
+import { theme } from '@/styles/theme'
 
-const AASdk = ({ title, caption, text, link, items }: BaseBlock) => {
+const AASdk = ({ title, caption, text, link }: BaseBlock) => {
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'))
+
   return (
     <Container className={css.wrapper}>
       <Grid container className={layoutCss.containerShort}>
@@ -31,7 +34,7 @@ const AASdk = ({ title, caption, text, link, items }: BaseBlock) => {
         </Grid>
         <div className={css.videoWrapper}>
           <video autoPlay muted loop className={css.video}>
-            <source src="/videos/aa-sdk.webm" type="video/webm" />
+            <source src={isSmallScreen ? '/videos/aa-sdk-mobile.webm' : '/videos/aa-sdk.webm'} type="video/webm" />
           </video>
           <a className={clsx(css.videoLink, css.authKitLink)} href={AUTH_KIT_LINK} target="_blank" rel="noreferrer">
             Auth Kit
