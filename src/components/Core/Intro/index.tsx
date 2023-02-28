8351a7d4048c67433bafda3dd50a9723d8b04c66
Usame Algan <usame@safe.global>
2023-02-28 14:59:21 +0100

    Add core intro video

--- src/components/Core/Intro/index.tsx
+++ src/components/Core/Intro/index.tsx
@@ -2,7 +2,7 @@ import { Button, Container, Divider, Grid, Typography } from '@mui/material'
 import css from './styles.module.css'
 import type { BaseBlock } from '@/components/Home/types'
 
-const CoreIntro = ({ title, text, image, link }: BaseBlock) => {
+const CoreIntro = ({ title, text, video, link }: BaseBlock & { video: { src: string } }) => {
   return (
     <Container>
       <Grid
@@ -10,9 +10,12 @@ const CoreIntro = ({ title, text, image, link }: BaseBlock) => {
         className={css.container}
         spacing={{ xs: 6, md: '30px', xl: '50px' }}
         justifyContent="space-between"
+        alignItems="center"
       >
         <Grid item md={6} display="flex" justifyContent="center">
-          <img className={css.coreImage} {...image} />
+          <video autoPlay muted loop className={css.video}>
+            <source src={video.src} type="video/webm" />
+          </video>
         </Grid>
         <Grid item md={6}>
           <Typography className={css.title} variant="h1" mb={4}>
