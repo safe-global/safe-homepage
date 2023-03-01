86a1cb125032531331fdf4f00b7be4090866629d
Usame Algan <usame@safe.global>
2023-03-01 10:25:49 +0100

    Add empty terms, privacy, cookie policy pages

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,12 @@
 export const AppRoutes = {
   '404': '/404',
   wallet: '/wallet',
+  terms: '/terms',
+  privacy: '/privacy',
   licenses: '/licenses',
   index: '/',
   imprint: '/imprint',
   core: '/core',
+  cookie: '/cookie',
   careers: '/careers',
 }
