fee82362fe32928f13964eb8ed4c682ae5f5aa09
Usame Algan <5880855+usame-algan@users.noreply.github.com>
2023-03-01 10:52:46 +0100

    Add empty terms, privacy, cookie policy pages (#146)

--- src/components/common/CookieBanner/index.tsx
+++ src/components/common/CookieBanner/index.tsx
@@ -6,6 +6,7 @@ import type { ReactElement } from 'react'
 import { useCookieBannerContext } from './CookieBannerContext'
 
 import css from './styles.module.css'
+import { AppRoutes } from '@/config/routes'
 
 export const enum CookieType {
   NECESSARY = 'necessary',
@@ -39,9 +40,9 @@ export const CookieBanner = (): ReactElement | null => {
       <div className={css.content}>
         <Typography align="center">
           We use cookies to provide you with the best experience and to help improve our website and application. Please
-          read our <Link href="/cookie">Cookie Policy</Link> for more information. By clicking &quot;Accept all&quot;,
-          you agree to the storing of cookies on your device to enhance site navigation, analyze site usage and provide
-          customer support.
+          read our <Link href={AppRoutes.cookie}>Cookie Policy</Link> for more information. By clicking &quot;Accept
+          all&quot;, you agree to the storing of cookies on your device to enhance site navigation, analyze site usage
+          and provide customer support.
         </Typography>
 
         <form className={css.grid}>
