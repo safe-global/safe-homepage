133fe3490ec99e53a75c1ee314112c5c5f5e9fc4
Usame Algan <usame@safe.global>
2023-04-04 14:41:18 +0200

    fix: Remove licenses page, update licenses link

--- src/pages/licenses.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import { Licenses } from '@/components/Licenses'
-
-const LicensesPage: NextPage = () => {
-  return <Licenses />
-}
-
-export default LicensesPage
