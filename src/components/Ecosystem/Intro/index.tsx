9a1c65e475bea8b5eb889576c94f23f147535f83
Usame Algan <usame@safe.global>
2023-05-09 16:47:57 +0200

    Add background particle bg to header

--- src/components/Ecosystem/Intro/index.tsx
+++ src/components/Ecosystem/Intro/index.tsx
@@ -1,10 +1,12 @@
 import { Container, Grid, Typography } from '@mui/material'
 import css from './styles.module.css'
+import BackgroundParticles from '@/public/images/particle-bg.svg'
 import type { BaseBlock } from '@/components/Home/types'
 
 const EcosystemIntro = ({ title, text }: BaseBlock) => {
   return (
     <Container>
+      <BackgroundParticles className={css.bg} />
       <Grid container className={css.container} spacing={{ xs: 6, md: '30px', xl: '50px' }}>
         <Grid item md={8}>
           <Typography className={css.title} variant="h1" mb={4}>
