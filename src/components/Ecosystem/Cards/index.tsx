71687a6417cec8bce0e74da75009ce53acc0f36c
Usame Algan <usame@safe.global>
2023-05-09 12:40:59 +0200

    Add integrate CTA to the ecosystem page

--- src/components/Ecosystem/Cards/index.tsx
+++ src/components/Ecosystem/Cards/index.tsx
@@ -2,20 +2,32 @@ import { type BaseBlock } from '@/components/Home/types'
 import css from './styles.module.css'
 import { Container, Grid, Typography } from '@mui/material'
 import Link from 'next/link'
+import clsx from 'clsx'
 
-const Cards = ({ items }: BaseBlock) => {
+const Cards = ({ items, stacked = false }: BaseBlock & { stacked?: boolean }) => {
   return (
     <Container>
       <Grid container>
         {items
           ? items.map(({ title, text, link, image }, index) => (
-              <Grid key={index} item xs={12} md={12 / items.length} className={css.card}>
+              <Grid
+                key={index}
+                item
+                xs={12}
+                md={stacked ? 12 : 12 / items.length}
+                className={clsx(css.card, stacked && css.stacked)}
+              >
                 <div className={css.cardContent}>
                   <Typography variant="h4" component="div" color="text.primary">
                     {title}
                   </Typography>
                   {image && <img src={image.src} alt={image.alt} className={css.cardImage} />}
                 </div>
+                {text && (
+                  <Typography color="primary.light" mt={1}>
+                    {text}
+                  </Typography>
+                )}
                 {link && (
                   <Link href={link.href} target="_blank" rel="noreferrer" className={css.cardLink}>
                     {link.title}
