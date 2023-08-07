fb8230e96426a3a726af3200552ac3ae3f25c7c1
Diogo Soares <diogo.lsoares@gmail.com>
2023-08-07 15:46:17 +0200

    add staging Hotjar ID

--- src/hooks/useHotjar.ts
+++ src/hooks/useHotjar.ts
@@ -1,17 +1,19 @@
 import { useEffect } from 'react'
 import { useCookieBannerContext } from '@/components/common/CookieBanner/CookieBannerContext'
-import { HOTJAR_ID, HOTJAR_VERSION, IS_PRODUCTION } from '@/config/constants'
+import { HOTJAR_ID, HOTJAR_ID_STAGING, HOTJAR_VERSION, IS_PRODUCTION } from '@/config/constants'
 
 let scriptRef: HTMLScriptElement | null = null
+
+const ID = IS_PRODUCTION ? HOTJAR_ID : HOTJAR_ID_STAGING
 
 const useHotjar = () => {
   const { isAnalyticsEnabled } = useCookieBannerContext()
 
   // Add/remove hotjar script
   useEffect(() => {
-    if (IS_PRODUCTION && isAnalyticsEnabled) {
+    if (isAnalyticsEnabled) {
       scriptRef = document.createElement('script')
-      scriptRef.src = `https://static.hotjar.com/c/hotjar-${HOTJAR_ID}.js?sv=${HOTJAR_VERSION}`
+      scriptRef.src = `https://static.hotjar.com/c/hotjar-${ID}.js?sv=${HOTJAR_VERSION}`
       scriptRef.async = true
       scriptRef.defer = true
       document.body.appendChild(scriptRef)
