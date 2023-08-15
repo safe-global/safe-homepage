d86453e9b814e3a03105d9897070cb3c76c58c43
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-08-15 10:53:40 +0200

    feat: add the "scroll" indicator on hovering the Intro section in the… (#228)
    * feat: add the "scroll" indicator on hovering the Intro section in the Core page
    
    * center scroll element
    
    * move Core's Intro a bit up
    
    * remove unused animation hook
    
    * change scroll element copy
    
    * change Core>Intro copy
    
    * make the scroll indicator clickable and smooth scroll to the masthead section

--- src/components/Core/index.tsx
+++ src/components/Core/index.tsx
@@ -1,10 +1,7 @@
 import type { ReactElement } from 'react'
 import PageContent from '@/components/common/PageContent'
 import coreContent from '@/content/core.json'
-import { useDelayedScrollAnimation } from '@/hooks/useDelayedScrollAnimation'
 
 export const Core = (): ReactElement => {
-  useDelayedScrollAnimation('masthead', 1000, 500, 0.2)
-
   return <PageContent content={coreContent} path="core.json" />
 }
