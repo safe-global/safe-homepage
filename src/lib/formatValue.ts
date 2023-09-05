59ec55210a9cb9e5dbb21c2e641f41c26f2e79db
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2023-09-05 15:22:51 +0200

    fix: formatter now rounds numbers down and displays only 1 decimal (#232)
    * feat: adapts the formatter to not round but rather truncate to one decimal digit
    
    * use `en-US` locale for number formatting
    
    * use switch over if..else if

--- src/lib/formatValue.ts
+++ src/lib/formatValue.ts
@@ -1,7 +1,43 @@
+/**
+ * Formats the given number in thousands order of magnitude and truncates the decimal part to 1 digit.
+ *
+ * @param {number} value - The number to be formatted.
+ * @returns {string} The formatted string representation of the number.
+ */
 export function formatValue(value: number): string {
-  const numberFormat = new Intl.NumberFormat(undefined, {
+  const numberFormat = new Intl.NumberFormat('en-US', {
     notation: 'compact',
+    maximumFractionDigits: 2,
   })
 
-  return `${numberFormat.format(value)}`
+  const parts = numberFormat.formatToParts(value)
+  let integer = '0'
+  let decimalPart = ''
+  let compact = ''
+
+  for (const part of parts) {
+    switch (part.type) {
+      case 'integer':
+        integer = part.value
+        break
+      case 'decimal':
+        if (integer.length === 1) {
+          decimalPart = part.value
+        }
+        break
+      case 'fraction':
+        if (integer.length === 1) {
+          // Truncates the fraction part to 1 decimal place if the value has a single integer digit
+          decimalPart = `${decimalPart}${part.value[0]}`
+        }
+        break
+      case 'compact':
+        compact = part.value
+        break
+      default:
+        break
+    }
+  }
+
+  return `${integer}${decimalPart}${compact}`
 }
