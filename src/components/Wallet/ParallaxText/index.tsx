14e1d98b7820a0bb3317e296f8745bf7ae36da41
Diogo Soares <diogo.lsoares@gmail.com>
2023-10-10 09:59:49 +0200

    refactor: use ButtonsWrapper

--- src/components/Wallet/ParallaxText/index.tsx
+++ src/components/Wallet/ParallaxText/index.tsx
@@ -1,12 +1,11 @@
 import type { ReactNode } from 'react'
-import { Box, Button, Container, Grid, Typography } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
 import layoutCss from '@/components/common/styles.module.css'
 import css from './styles.module.css'
-import LinkButton from '@/components/common/LinkButton'
 import type { BaseBlock } from '@/components/Home/types'
 import Stepper, { type StepsType } from '@/components/Wallet/Stepper'
-import Link from 'next/link'
 import clsx from 'clsx'
+import ButtonsWrapper from '@/components/common/ButtonsWrapper'
 
 export type ParallaxTextProps = BaseBlock & {
   variant: 'image-text' | 'text-image'
@@ -38,26 +37,7 @@ const ParallaxText = ({
           <Typography variant="h2">{title}</Typography>
           <Typography>{text}</Typography>
           {steps && <Stepper steps={steps} />}
-          {buttons ? (
-            <Box display="flex" gap={3}>
-              {buttons.map((button, index) => {
-                const { text, variant, href } = button
-                const isButton = variant === 'button'
-                if (isButton) {
-                  return (
-                    <Button key={index} href={href} variant="contained" size="large" target="_blank" rel="noreferrer">
-                      {text}
-                    </Button>
-                  )
-                }
-                return (
-                  <Link key={index} href={href} passHref target="_blank" rel="noreferrer">
-                    <LinkButton>{text}</LinkButton>
-                  </Link>
-                )
-              })}
-            </Box>
-          ) : null}
+          <ButtonsWrapper buttons={buttons} />
         </Grid>
         <Grid item xs={12} md={6} display="flex" alignItems="center">
           {children}
