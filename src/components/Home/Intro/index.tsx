8ff3a48ee3764919b102630b606a44ef43b4042d
Diogo Soares <diogo.lsoares@gmail.com>
2023-10-11 14:33:15 +0200

    fix: move text block to respect the spacings

--- src/components/Home/Intro/index.tsx
+++ src/components/Home/Intro/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Typography } from '@mui/material'
+import { Grid, Typography } from '@mui/material'
 import css from '@/components/Home/Intro/styles.module.css'
 import type { BaseBlock } from '@/components/Home/types'
 import ButtonsWrapper from '@/components/common/ButtonsWrapper'
@@ -7,19 +7,17 @@ const Intro = ({ title, text, buttons }: BaseBlock) => {
   return (
     <div className={css.container}>
       <div className={css.wrapper}>
-        <Container>
-          <Grid container flexDirection="column" className={css.content}>
-            <Grid item md={10} width="100%">
-              <Typography variant="h1" mb={3}>
-                {title}
-              </Typography>
-            </Grid>
-            <Grid item md={6}>
-              <Typography className={css.subtitle}>{text}</Typography>
-              <ButtonsWrapper buttons={buttons} mobileDirection="row" />
-            </Grid>
+        <Grid container className={css.content}>
+          <Grid item md={8} width="100%">
+            <Typography variant="h1" mb={3}>
+              {title}
+            </Typography>
           </Grid>
-        </Container>
+          <Grid item md={8}>
+            <Typography className={css.subtitle}>{text}</Typography>
+            <ButtonsWrapper buttons={buttons} mobileDirection="row" />
+          </Grid>
+        </Grid>
         <div className={css.filter}>
           <video autoPlay muted loop className={css.video}>
             <source src="/videos/safe-logo.mp4" type="video/mp4" />
