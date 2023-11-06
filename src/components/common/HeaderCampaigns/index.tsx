4970f961f638b71224c8e7187976747a3582dd4c
Diogo Soares <diogo.lsoares@gmail.com>
2023-11-06 12:54:03 +0100

    feat: component Campaigns header

---
+++ src/components/common/HeaderCampaigns/index.tsx
@@ -0,0 +1,12 @@
+import SafeWalletLogo from '@/public/images/safe-wallet-logo.svg'
+import css from './styles.module.css'
+
+const HeaderCampaigns = () => {
+  return (
+    <div className={css.header}>
+      <SafeWalletLogo />
+    </div>
+  )
+}
+
+export default HeaderCampaigns
