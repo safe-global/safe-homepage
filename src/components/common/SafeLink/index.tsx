fe82e494c6082142cf1b0482371206117e6797fd
Diogo Soares <diogo.lsoares@gmail.com>
2023-12-14 15:59:28 +0100

    memoize href on SafeLink

--- src/components/common/SafeLink/index.tsx
+++ src/components/common/SafeLink/index.tsx
@@ -1,15 +1,15 @@
 import SearchParamsContext from '@/contexts/SearchParamsContext'
-import { type ReactNode, useContext } from 'react'
+import { type ReactNode, useContext, useMemo } from 'react'
 import Link from 'next/link'
 import { appendSearchParamsToURL } from '@/lib/appendSearchParamsToURL'
 
 const SafeLink = ({ href, children }: { href: string; children: ReactNode }) => {
   const searchParams = useContext(SearchParamsContext)
 
-  const finalHref = appendSearchParamsToURL(href, searchParams)
+  const finalHref = useMemo(() => appendSearchParamsToURL(href, searchParams), [href, searchParams])
 
   return (
-    <Link href={finalHref} target="_blank" rel="noreferrer">
+    <Link href={finalHref} target="_blank">
       {children}
     </Link>
   )
