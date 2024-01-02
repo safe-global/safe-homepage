6927cb52ac1785fc8785bb7eec09e1d56421f1b0
Diogo Soares <diogo.lsoares@gmail.com>
2024-01-02 16:26:08 +0100

    fix: project card categories overflow

--- src/components/Ecosystem/Projects/ProjectCard.tsx
+++ src/components/Ecosystem/Projects/ProjectCard.tsx
@@ -6,62 +6,60 @@ import GithubIcon from '@/public/images/github-icon.svg'
 import css from './styles.module.css'
 import { ECOSYSTEM_DATA_URL } from '@/config/constants'
 import { type EcosystemProjectWithCategories } from '@/hooks/useEcosystemData'
-import Link from 'next/link'
+import clsx from 'clsx'
 
 export const ProjectCard = (item: EcosystemProjectWithCategories) => {
   const categories = item.categories_list
 
   return (
-    <div className={css.card}>
-      <Avatar className={css.image} src={ECOSYSTEM_DATA_URL + item.logo}>
-        &nbsp;
-      </Avatar>
+    <a href={item.project_website} target="_blank" rel="noreferrer">
+      <div className={clsx(css.card, { [css.outline]: item.project_website })}>
+        <Avatar className={css.image} src={ECOSYSTEM_DATA_URL + item.logo}>
+          &nbsp;
+        </Avatar>
 
-      <div className={css.socials}>
-        {item.twitter && (
-          <IconButton
-            aria-label="Twitter link"
-            href={item.twitter}
-            target="_blank"
-            rel="noreferrer"
-            size="small"
-            sx={{ color: 'primary.light' }}
-          >
-            <SvgIcon component={XIcon} fontSize="inherit" color="inherit" inheritViewBox />
-          </IconButton>
-        )}
+        <div className={css.socials}>
+          {item.twitter && (
+            <IconButton
+              aria-label="Twitter link"
+              href={item.twitter}
+              target="_blank"
+              rel="noreferrer"
+              size="small"
+              sx={{ color: 'primary.light' }}
+            >
+              <SvgIcon component={XIcon} fontSize="inherit" color="inherit" inheritViewBox />
+            </IconButton>
+          )}
 
-        {item.github_dev_docs && (
-          <IconButton
-            aria-label="Github link"
-            href={item.github_dev_docs}
-            target="_blank"
-            rel="noreferrer"
-            size="small"
-            sx={{ color: 'primary.light' }}
-          >
-            <SvgIcon component={GithubIcon} fontSize="inherit" color="inherit" inheritViewBox />
-          </IconButton>
-        )}
+          {item.github_dev_docs && (
+            <IconButton
+              aria-label="Github link"
+              href={item.github_dev_docs}
+              target="_blank"
+              rel="noreferrer"
+              size="small"
+              sx={{ color: 'primary.light' }}
+            >
+              <SvgIcon component={GithubIcon} fontSize="inherit" color="inherit" inheritViewBox />
+            </IconButton>
+          )}
+        </div>
+
+        <Typography fontWeight="500" mb={0.5}>
+          {item.project}
+        </Typography>
+
+        <Typography variant="body2" className={css.description}>
+          {item.description}
+        </Typography>
+
+        <div className={css.categories}>
+          {categories.map((category, idx) => {
+            return <Chip key={category + idx} className={css.chip} label={category} />
+          })}
+        </div>
       </div>
-
-      {item.project_website && (
-        <Link className={css.cardLink} href={item.project_website} target="_blank" rel="noreferrer" />
-      )}
-
-      <Typography fontWeight="500" mb={0.5}>
-        {item.project}
-      </Typography>
-
-      <Typography variant="body2" className={css.description}>
-        {item.description}
-      </Typography>
-
-      <div className={css.categories}>
-        {categories.map((category, idx) => {
-          return <Chip key={category + idx} className={css.chip} label={category} />
-        })}
-      </div>
-    </div>
+    </a>
   )
 }
