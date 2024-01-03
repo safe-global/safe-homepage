a4a1767913cca63e2f027fb2d9c20ce119f7bf82
Usame Algan <usame@safe.global>
2024-01-03 15:14:18 +0100

    fix: Move social links out of the inner card and wrap card content with an a

--- src/components/Ecosystem/Projects/ProjectCard.tsx
+++ src/components/Ecosystem/Projects/ProjectCard.tsx
@@ -11,55 +11,64 @@ import clsx from 'clsx'
 export const ProjectCard = (item: EcosystemProjectWithCategories) => {
   const categories = item.categories_list
 
-  return (
-    <a href={item.project_website} target="_blank" rel="noreferrer">
-      <div className={clsx(css.card, { [css.outline]: item.project_website })}>
-        <Avatar className={css.image} src={ECOSYSTEM_DATA_URL + item.logo}>
-          &nbsp;
-        </Avatar>
+  const CardContent = (
+    <div className={clsx(css.card, { [css.outline]: item.project_website })}>
+      <Avatar className={css.image} src={ECOSYSTEM_DATA_URL + item.logo}>
+        &nbsp;
+      </Avatar>
 
-        <div className={css.socials}>
-          {item.twitter && (
-            <IconButton
-              aria-label="Twitter link"
-              href={item.twitter}
-              target="_blank"
-              rel="noreferrer"
-              size="small"
-              sx={{ color: 'primary.light' }}
-            >
-              <SvgIcon component={XIcon} fontSize="inherit" color="inherit" inheritViewBox />
-            </IconButton>
-          )}
+      <Typography fontWeight="500" mb={0.5}>
+        {item.project}
+      </Typography>
 
-          {item.github_dev_docs && (
-            <IconButton
-              aria-label="Github link"
-              href={item.github_dev_docs}
-              target="_blank"
-              rel="noreferrer"
-              size="small"
-              sx={{ color: 'primary.light' }}
-            >
-              <SvgIcon component={GithubIcon} fontSize="inherit" color="inherit" inheritViewBox />
-            </IconButton>
-          )}
-        </div>
+      <Typography variant="body2" className={css.description}>
+        {item.description}
+      </Typography>
 
-        <Typography fontWeight="500" mb={0.5}>
-          {item.project}
-        </Typography>
-
-        <Typography variant="body2" className={css.description}>
-          {item.description}
-        </Typography>
-
-        <div className={css.categories}>
-          {categories.map((category, idx) => {
-            return <Chip key={category + idx} className={css.chip} label={category} />
-          })}
-        </div>
+      <div className={css.categories}>
+        {categories.map((category, idx) => {
+          return <Chip key={category + idx} className={css.chip} label={category} />
+        })}
       </div>
-    </a>
+    </div>
+  )
+
+  return (
+    <div className={css.cardWrapper}>
+      <div className={css.socials}>
+        {item.twitter && (
+          <IconButton
+            aria-label="Twitter link"
+            href={item.twitter}
+            target="_blank"
+            rel="noreferrer"
+            size="small"
+            sx={{ color: 'primary.light' }}
+          >
+            <SvgIcon component={XIcon} fontSize="inherit" color="inherit" inheritViewBox />
+          </IconButton>
+        )}
+
+        {item.github_dev_docs && (
+          <IconButton
+            aria-label="Github link"
+            href={item.github_dev_docs}
+            target="_blank"
+            rel="noreferrer"
+            size="small"
+            sx={{ color: 'primary.light' }}
+          >
+            <SvgIcon component={GithubIcon} fontSize="inherit" color="inherit" inheritViewBox />
+          </IconButton>
+        )}
+      </div>
+      {item.project_website ? (
+        <a href={item.project_website} target="_blank" rel="noreferrer">
+          {CardContent}
+        </a>
+      ) : (
+        CardContent
+      )}
+    </div>
   )
 }
