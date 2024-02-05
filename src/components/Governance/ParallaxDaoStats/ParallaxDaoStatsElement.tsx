c695528f70c9ec2ff33f39deb1937329205dcf9d
Diogo Soares <diogo.lsoares@gmail.com>
2024-02-05 11:03:27 +0100

    refactor: replace images

--- src/components/Governance/ParallaxDaoStats/ParallaxDaoStatsElement.tsx
+++ src/components/Governance/ParallaxDaoStats/ParallaxDaoStatsElement.tsx
@@ -1,23 +1,35 @@
-import Image from 'next/image'
 import FrameImage from '@/public/images/Governance/Parallaxes/DaoStats/background.svg'
-import ProposalsImage from '@/public/images/Governance/Parallaxes/DaoStats/proposals.png'
-import DelegatesImage from '@/public/images/Governance/Parallaxes/DaoStats/delegates.png'
-import DelegatorsImage from '@/public/images/Governance/Parallaxes/DaoStats/delegators.png'
 import ParallaxWrapper from '@/components/common/ParallaxWrapper'
 import css from './styles.module.css'
+import { Typography } from '@mui/material'
 
 const ParallaxDaoStatsElement = () => {
   return (
     <div className={css.parallaxWrapper}>
       <FrameImage className={css.baseImage} />
       <ParallaxWrapper translateX={0} translateY={0} depth={2} direction={-1}>
-        <Image src={ProposalsImage} alt="8 proposals" className={css.proposals} />
+        <div className={`${css.card} ${css.proposals}`}>
+          <Typography className={css.value}>8</Typography>
+          <Typography variant="caption" className={css.caption}>
+            proposals
+          </Typography>
+        </div>
       </ParallaxWrapper>
       <ParallaxWrapper translateX={0} translateY={0} depth={1} direction={-1}>
-        <Image src={DelegatesImage} alt="2.5K delegates" className={css.delegates} />
+        <div className={`${css.card} ${css.delegates}`}>
+          <Typography className={css.value}>2.5K</Typography>
+          <Typography variant="caption" className={css.caption}>
+            delegates
+          </Typography>
+        </div>
       </ParallaxWrapper>
       <ParallaxWrapper translateX={0} translateY={0} depth={0} direction={-1}>
-        <Image src={DelegatorsImage} alt="11.2K delegators" className={css.delegators} />
+        <div className={`${css.card} ${css.delegators}`}>
+          <Typography className={css.value}>11.2K</Typography>
+          <Typography variant="caption" className={css.caption}>
+            delegators
+          </Typography>
+        </div>
       </ParallaxWrapper>
     </div>
   )
