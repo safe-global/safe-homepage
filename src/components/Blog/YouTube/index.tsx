620cbf644247b1ae7db1cf7628a0db1c52c0cafb
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-03-04 11:33:47 +0100

    fix: display Youtube playlists in RichText (#280)
    * fix: enhance isYoutubeUrl to accept videos from playlists
    
    * embed playlists in the YouTube iframe
    
    * Refactor extract search params function. Prefer URL object methods over ternaries.

--- src/components/Blog/YouTube/index.tsx
+++ src/components/Blog/YouTube/index.tsx
@@ -1,17 +1,17 @@
-import { extractYouTubeVideoId } from '@/lib/urlPatterns'
+import { getYoutubeVideoSrc } from '@/lib/urlPatterns'
 import css from './styles.module.css'
 
 const YouTube = ({ url }: { url: string }) => {
-  const videoId = extractYouTubeVideoId(url)
+  const videoSrc = getYoutubeVideoSrc(url)
 
-  if (!videoId) return null
+  if (!videoSrc) return null
 
   return (
     <div className={css.videoResponsive}>
       <iframe
         width="853"
         height="480"
-        src={`https://www.youtube-nocookie.com/embed/${videoId}`}
+        src={videoSrc}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
         title="Embedded YouTube"
       />
