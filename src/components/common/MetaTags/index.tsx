3a10fc3b10df6070151dce1bcacdfc57f916d6b3
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-03-20 14:31:26 +0100

    chore: update favicons (#287)
    * update favicons
    
    * Add the manifest files
    
    * Move metatags to dedicated component

--- src/components/common/MetaTags/index.tsx
+++ src/components/common/MetaTags/index.tsx
@@ -30,6 +30,20 @@ const MetaTags = (props: Partial<typeof defaultMetaTags>) => {
       <meta name="twitter:image" content={seo.image} />
 
       {!IS_PRODUCTION && <meta name="robots" content="noindex" />}
+
+      {/* Mobile tags */}
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-title" content="safe" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+      {/* Favicons */}
+      <link rel="shortcut icon" href="/favicons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" />
+      <link rel="manifest" href="/favicons/site.webmanifest" />
+      <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
     </Head>
   )
 }
