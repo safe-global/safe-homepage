6a99c0418e3ce7d697730d1c7912f029173eb511
Diogo Soares <diogo.lsoares@gmail.com>
2024-03-21 10:04:21 +0100

    move preview page inside /preview folder

--- src/pages/blog/[slug]/preview/index.tsx
+++ src/pages/blog/[slug]/preview/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 import { previewClient } from '@/lib/contentful'
 import { type TypePostSkeleton } from '@/contentful/types'
 import BlogPost, { type BlogPostEntry } from '@/components/Blog/Post'
+import { type NextPage } from 'next'
 
 const fetchDraftContent = async (slug: string) => {
   const content = await previewClient.getEntries<TypePostSkeleton>({
@@ -13,7 +14,7 @@ const fetchDraftContent = async (slug: string) => {
   return content.items[0]
 }
 
-const Preview = () => {
+const Preview: NextPage = () => {
   const router = useRouter()
   const { slug, secret } = router.query as { slug: string; secret: string }
   const [blogPost, setBlogPost] = useState<BlogPostEntry | null>(null)
