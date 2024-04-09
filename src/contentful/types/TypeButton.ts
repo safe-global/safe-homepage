df9b2c873ad46feea5dc49a2a980204cffde8ad2
Diogo Soares <diogo.lsoares@gmail.com>
2024-04-10 00:12:18 +0200

    ButtonsWrapper

--- src/contentful/types/TypeButton.ts
+++ src/contentful/types/TypeButton.ts
@@ -1,8 +1,9 @@
 import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from 'contentful'
 
 export interface TypeButtonFields {
-  btnCopy: EntryFieldTypes.Symbol
-  btnHref: EntryFieldTypes.Symbol
+  text: EntryFieldTypes.Symbol
+  href: EntryFieldTypes.Symbol
+  variant: EntryFieldTypes.Symbol<'button' | 'link'>
 }
 
 export type TypeButtonSkeleton = EntrySkeletonType<TypeButtonFields, 'button'>
