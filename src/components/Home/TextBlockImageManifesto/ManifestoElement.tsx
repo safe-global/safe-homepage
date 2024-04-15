7057c63ffeca67a71305041632656128de2af2bb
Diogo Soares <diogo.lsoares@gmail.com>
2024-04-16 00:37:54 +0200

    feat: Ecosystem section

---
+++ src/components/Home/TextBlockImageManifesto/ManifestoElement.tsx
@@ -0,0 +1,13 @@
+import { type BaseBlock } from '@/components/Home/types'
+import css from './styles.module.css'
+
+const ManifestoElement = ({ image }: { image: BaseBlock['image'] }) => {
+  return (
+    <div className={css.imageWrapper}>
+      <img src="/images/Home/manifesto-bg.png" alt="Particles background" className={css.bgImage} />
+      {image ? <img src={image.src} alt={image.alt} className={css.image} /> : null}
+    </div>
+  )
+}
+
+export default ManifestoElement
--- src/components/common/TextBlockImage/index.tsx
+++ src/components/common/TextBlockImage/index.tsx
@@ -8,9 +8,10 @@ import css from './styles.module.css'
 export type TextBlockImageProps = BaseBlock & {
   variant: 'image-text' | 'text-image'
   mobileVariant?: 'image-text' | 'text-image'
+  children: React.ReactNode
 }
 
-const TextBlockImage = ({ caption, title, text, buttons, image, variant, mobileVariant }: TextBlockImageProps) => {
+const TextBlockImage = ({ caption, title, text, buttons, variant, mobileVariant, children }: TextBlockImageProps) => {
   return (
     <Container>
       <Grid
@@ -20,25 +21,24 @@ const TextBlockImage = ({ caption, title, text, buttons, image, variant, mobileV
           variant === 'image-text' ? css.imageFirst : css.textFirst,
           mobileVariant === 'text-image' ? css.textFirstMobile : null,
         )}
-        spacing={{ xs: 6, md: '30px', xl: '50px' }}
+        spacing={{ xs: '30px', xl: '50px' }}
         justifyContent="space-between"
       >
-        <Grid item xs={12} md={5} display="flex" flexDirection="column" justifyContent="center" gap={{ xs: 3, md: 4 }}>
+        <Grid item xs={12} md={6} display="flex" flexDirection="column" justifyContent="center" gap={{ xs: 3, md: 4 }}>
           {caption && (
             <Typography variant="caption" component="div">
               {caption}
             </Typography>
           )}
           <Typography variant="h2">{title}</Typography>
-          <Typography className={css.textBlock}>{text}</Typography>
+          <Typography variant="h5" className={css.textBlock}>
+            {text}
+          </Typography>
           <ButtonsWrapper buttons={buttons} />
         </Grid>
 
-        <Grid item xs={12} md={6} display="flex" alignItems="center">
-          <div className={css.imageWrapper}>
-            <img src="/images/Home/manifesto-bg.png" alt="Particles background" className={css.bgImage} />
-            {image ? <img src={image.src} alt={image.alt} className={css.image} /> : null}
-          </div>
+        <Grid item xs={12} md={6}>
+          {children}
         </Grid>
       </Grid>
     </Container>
