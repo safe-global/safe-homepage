25d24eccccfd2706c5f0036e7c203abc1a66d001
Diogo Soares <diogo.lsoares@gmail.com>
2024-04-16 23:34:39 +0200

    fix: set background images width

--- src/components/Home/CompactGridSpacedCards/index.tsx
+++ src/components/Home/CompactGridSpacedCards/index.tsx
@@ -4,7 +4,7 @@ const CompactGridSpacedCards = () => {
   return (
     <Container>
       <Box height="100vh" display="flex" alignItems="center" justifyContent="center">
-        <Typography variant="h1">CompactGridSpacedCards</Typography>
+        <Typography variant="h1">Compact Grid Spaced Cards</Typography>
       </Box>
     </Container>
   )
--- src/components/Home/TexBlockImageIcons/index.tsx
+++ src/components/Home/TexBlockImageIcons/index.tsx
@@ -4,7 +4,7 @@ const TexBlockImageIcons = () => {
   return (
     <Container>
       <Box height="100vh" display="flex" alignItems="center" justifyContent="center">
-        <Typography variant="h1">TexBlockImageIcons</Typography>
+        <Typography variant="h1">Tex Block Image Icons</Typography>
       </Box>
     </Container>
   )
