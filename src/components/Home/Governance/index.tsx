e252f90ac7d2dc72f95c9f096a18d33efb6d992a
Diogo Soares <diogo.lsoares@gmail.com>
2024-04-17 01:10:45 +0200

    apply a gradient to the Governance section bg

--- src/components/Home/Governance/index.tsx
+++ src/components/Home/Governance/index.tsx
@@ -2,36 +2,59 @@ import { Box, Container, Grid, Typography } from '@mui/material'
 import { type BaseBlock } from '@/components/Home/types'
 import layoutCss from '@/components/common/styles.module.css'
 import css from './styles.module.css'
-import LinkCard from '@/components/common/LinkCard'
+import LinkButton from '@/components/common/LinkButton'
+import ArrowIcon from '@/public/images/arrow-out-icon.svg'
+
+const LinkCard = ({ caption, title, link }: Partial<BaseBlock>) => (
+  <div className={`${css.card} ${css.outline}`}>
+    <div className={css.cardHeader}>{caption ? <Typography variant="caption">{caption}</Typography> : undefined}</div>
+
+    <div className={css.cardBody}>
+      <Typography variant="h3" className={css.title}>
+        {title}
+      </Typography>
+
+      {link ? (
+        <LinkButton underline={false} fullSize>
+          {link.title}
+        </LinkButton>
+      ) : undefined}
+    </div>
+
+    <ArrowIcon className={css.arrow} />
+  </div>
+)
 
 const Governance = ({ caption, title, text, items }: BaseBlock) => {
   return (
-    <div className={css.bg}>
-      <Container className={`${layoutCss.containerMedium} ${css.container}`}>
-        <Grid container columnSpacing={2} rowGap="20px">
-          <Grid item xs={12} md={6}>
-            <Typography variant="caption">{caption}</Typography>
-            <Box mt={{ xs: '20px', md: '48px' }}>
-              <Typography variant="h2">{title}</Typography>
-            </Box>
+    <div className={css.gradient}>
+      <div className={css.bg}>
+        <Container className={`${layoutCss.containerMedium} ${css.container}`}>
+          <Grid container columnSpacing={2} rowGap="20px">
+            <Grid item xs={12} md={6}>
+              <Typography variant="caption">{caption}</Typography>
+              <Box mt={{ xs: '20px', md: '48px' }}>
+                <Typography variant="h2">{title}</Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6} display="flex" alignItems="center">
+              <Typography color="primary.light">{text}</Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={6} display="flex" alignItems="center">
-            <Typography color="primary.light">{text}</Typography>
+
+          <Grid container columnSpacing={2} rowGap="30px" mt={{ xs: '40px', md: '80px' }}>
+            {items?.map(({ caption, title, link }, index) => {
+              if (!caption || !title || !link) return null
+
+              return (
+                <Grid key={index} item xs={12} md={4}>
+                  <LinkCard caption={caption} title={title} link={link} />
+                </Grid>
+              )
+            })}
           </Grid>
-        </Grid>
-
-        <Grid container columnSpacing={2} rowGap="30px" mt={{ xs: '40px', md: '80px' }}>
-          {items?.map(({ caption, title, link }, index) => {
-            if (!caption || !title || !link) return null
-
-            return (
-              <Grid key={index} item xs={12} md={4}>
-                <LinkCard caption={caption} title={title} link={link} />
-              </Grid>
-            )
-          })}
-        </Grid>
-      </Container>
+        </Container>
+      </div>
     </div>
   )
 }
