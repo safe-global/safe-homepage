3ee5b09e20f7dd2bbb87718408606a63ce25cf5e
Diogo Soares <diogo.lsoares@gmail.com>
2024-04-17 16:24:59 +0200

    add hover effect to Governance cards

--- src/components/Home/Governance/index.tsx
+++ src/components/Home/Governance/index.tsx
@@ -6,22 +6,28 @@ import LinkButton from '@/components/common/LinkButton'
 import ArrowIcon from '@/public/images/arrow-out-icon.svg'
 
 const LinkCard = ({ caption, title, link }: Partial<BaseBlock>) => (
-  <div className={`${css.card} ${css.outline}`}>
-    <div className={css.cardHeader}>{caption ? <Typography variant="caption">{caption}</Typography> : undefined}</div>
+  <div className={css.cardWrapper}>
+    <div className={css.outline}>
+      <div className={css.card}>
+        <div className={css.cardHeader}>
+          {caption ? <Typography variant="caption">{caption}</Typography> : undefined}
+        </div>
 
-    <div className={css.cardBody}>
-      <Typography variant="h3" className={css.title}>
-        {title}
-      </Typography>
+        <div className={css.cardBody}>
+          <Typography variant="h3" className={css.title}>
+            {title}
+          </Typography>
 
-      {link ? (
-        <LinkButton underline={false} fullSize>
-          {link.title}
-        </LinkButton>
-      ) : undefined}
+          {link ? (
+            <LinkButton underline={false} fullSize>
+              {link.title}
+            </LinkButton>
+          ) : undefined}
+        </div>
+
+        <ArrowIcon className={css.arrow} />
+      </div>
     </div>
-
-    <ArrowIcon className={css.arrow} />
   </div>
 )
 
@@ -32,7 +38,9 @@ const Governance = ({ caption, title, text, items }: BaseBlock) => {
         <Container className={`${layoutCss.containerMedium} ${css.container}`}>
           <Grid container columnSpacing={2} rowGap="20px">
             <Grid item xs={12} md={6}>
-              <Typography variant="caption">{caption}</Typography>
+              <Typography variant="caption" className={css.caption}>
+                {caption}
+              </Typography>
               <Box mt={{ xs: '20px', md: '48px' }}>
                 <Typography variant="h2">{title}</Typography>
               </Box>
