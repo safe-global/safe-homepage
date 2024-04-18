c140c8603c4f9203553d2d546734526692e3c35d
Diogo Soares <diogo.lsoares@gmail.com>
2024-04-18 11:20:48 +0200

    add links to use cases

--- src/components/Token/ParallaxToken/ParallaxUseCasesElement.tsx
+++ src/components/Token/ParallaxToken/ParallaxUseCasesElement.tsx
@@ -4,6 +4,8 @@ import { Typography } from '@mui/material'
 import { type Entry } from 'contentful'
 import { type TypeBaseBlockSkeleton } from '@/contentful/types'
 import css from './styles.module.css'
+import { isEntryTypeExternalURL } from '@/lib/typeGuards'
+import LinkButton from '@/components/common/LinkButton'
 
 const blockClassNames = ['stepZero', 'stepOne', 'stepTwo', 'stepThree', 'stepFour', 'stepFive', 'stepSix']
 const depths = [0, 1, 2] as const
@@ -19,14 +21,21 @@ const ParallaxGovernanceProcessElement = ({ items }: { items: Entry<TypeBaseBloc
   <div className={css.parallaxWrapper}>
     <FrameImage className={css.baseImage} />
 
-    {items.map((_, index) => {
+    {items.map((item, index) => {
+      const { caption, link } = item.fields
+
       const className = blockClassNames[index]
       const depth = depths[index % 2]
 
       return (
         <ParallaxWrapper translateX={0} translateY={0} depth={depth} direction={-1} key={index}>
           <div className={`${css.movingElement} ${css[className]}`}>
-            <Typography>{items[index].fields.caption}</Typography>
+            <Typography>
+              {link && isEntryTypeExternalURL(link) ? (
+                <LinkButton href={link.fields.url} fullSize underline={false} />
+              ) : undefined}
+              {caption}
+            </Typography>
             {trailingShadow}
           </div>
         </ParallaxWrapper>
