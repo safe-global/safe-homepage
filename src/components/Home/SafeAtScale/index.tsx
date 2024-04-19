927e38353c7691bd533f58806405f17aa83cd97f
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-04-19 15:55:03 +0200

    feat: Metrics section (#320)
    * feat: SafeAtScale animation impl & bracket video assets
    
    * move types to component file
    
    * text-align center
    
    ---------
    
    Co-authored-by: Fern Hook <fern@antireal.info>

--- src/components/Home/SafeAtScale/index.tsx
+++ src/components/Home/SafeAtScale/index.tsx
@@ -1,19 +1,88 @@
 import { type BaseBlock } from '@/components/Home/types'
-import { Container, Typography } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
 import layoutCss from '@/components/common/styles.module.css'
 import css from './styles.module.css'
+import { Fragment, useRef, type CSSProperties } from 'react'
+import { useSafeStats } from '@/hooks/useSafeStats'
+import useScrollReveal from '@/hooks/useScrollReveal'
+import TickerElement from './TickerElement'
 
-const SafeAtScale = ({ caption, title }: BaseBlock) => {
+const jsxOrStringToString = (s: string | JSX.Element): string =>
+  s instanceof String ? s : (s as JSX.Element).props.content
+
+const SafeAtScale = ({ caption, title, items }: BaseBlock) => {
+  const safeStats = useSafeStats()
+
+  const containerRef = useRef<HTMLDivElement>(null)
+  const visibleInViewport = useScrollReveal(containerRef, 50, true)
+
   return (
     <div className={css.gradient}>
       <div className={css.bg}>
-        <Container className={`${layoutCss.containerMedium} ${layoutCss.centeredContent}`}>
-          <Typography variant="caption" className={css.caption}>
-            {caption}
-          </Typography>
-          <Typography variant="h2" className={css.title}>
-            {title}
-          </Typography>
+        <Container ref={containerRef}>
+          <Grid container className={`${css.content} ${layoutCss.containerMedium} ${layoutCss.centeredContent}`}>
+            <Typography variant="caption" className={css.caption}>
+              {caption}
+            </Typography>
+            <Typography variant="h2" className={css.title}>
+              {title}
+            </Typography>
+            <div
+              className={`${css.metricsWrapper} ${layoutCss.centeredContent} ${
+                visibleInViewport ? css.open : css.closed
+              }`}
+            >
+              <div className={css.brackets}>
+                <div className={css.l}>
+                  <video autoPlay muted playsInline loop className={css.video}>
+                    <source src="/videos/Home/brackets-left-hevc.mov" type="video/quicktime; codecs=hvc1" />
+                    <source src="/videos/Home/brackets-left-vp9.webm" type="video/webm" />
+                  </video>
+                </div>
+                <div className={css.metricsSpacer}></div>
+                <div className={css.r}>
+                  <video autoPlay muted playsInline loop className={css.video}>
+                    <source src="/videos/Home/brackets-right-hevc.mov" type="video/quicktime; codecs=hvc1" />
+                    <source src="/videos/Home/brackets-right-vp9.webm" type="video/webm" />
+                  </video>
+                </div>
+              </div>
+              <div className={css.bracketSpacer}></div>
+              <div className={css.metricsInner}>
+                {items?.map((item, index) => {
+                  const fallback = jsxOrStringToString(item.title ?? '0')
+                  const value = safeStats[index] ?? fallback
+                  const key = jsxOrStringToString(item.text ?? index.toString())
+
+                  return (
+                    <Fragment key={key}>
+                      <a
+                        href={item.link?.href}
+                        target="_blank"
+                        rel="noreferrer"
+                        className={css.metric}
+                        style={{ '--i': index } as CSSProperties}
+                        key={'metric-' + index}
+                      >
+                        <TickerElement
+                          run={visibleInViewport}
+                          className={css.value}
+                          toValue={value}
+                          rollDurationSecs={1.5}
+                          delayTimeMs={350 + 150 * index}
+                          key={key}
+                        />
+                        <Typography variant="caption" className={css.caption}>
+                          {item.text}
+                        </Typography>
+                      </a>
+                    </Fragment>
+                  )
+                })}
+              </div>
+              <div className={css.bracketSpacer}></div>
+            </div>
+          </Grid>
         </Container>
       </div>
     </div>
---
+++ src/hooks/useScrollReveal.ts
@@ -0,0 +1,35 @@
+import type { RefObject } from 'react'
+import { useEffect, useState } from 'react'
+
+const useScrollReveal = (elementRef: RefObject<HTMLElement>, percentVisible: number, once: boolean = false) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const [completed, setCompleted] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (completed) return
+
+      const windowHeight = window.innerHeight
+
+      if (elementRef.current) {
+        const rect = elementRef.current.getBoundingClientRect()
+        const visible = !(
+          Math.floor(100 - ((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100) < percentVisible ||
+          Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+        )
+        setIsVisible(visible)
+        if (visible && once) setCompleted(true)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [elementRef, percentVisible, once, isVisible, completed])
+
+  return isVisible
+}
+
+export default useScrollReveal
