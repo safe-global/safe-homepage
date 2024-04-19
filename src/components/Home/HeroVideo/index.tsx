f715bc0842acd394b62050919d24582b50a638a5
Fern Hook <fern@antireal.info>
2024-04-19 06:03:43 +0100

    fix: hero video jump on load, video background issues

--- src/components/Home/HeroVideo/index.tsx
+++ src/components/Home/HeroVideo/index.tsx
@@ -1,16 +1,39 @@
 import { Grid, Typography } from '@mui/material'
-import { type BaseBlock } from '@/components/Home/types'
 import css from '@/components/Home/HeroVideo/styles.module.css'
+import type { BaseBlock } from '@/components/Home/types'
+import { useEffect, useRef, useState } from 'react'
 
-const HeroVideo = ({ title, link }: BaseBlock) => {
+const HeroVideo = ({ title }: BaseBlock) => {
+  const [ready, setReady] = useState(false)
+
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  useEffect(() => {
+    const poll = setInterval(() => {
+      if (!videoRef.current) return
+      if (videoRef.current.readyState >= videoRef.current.HAVE_FUTURE_DATA) {
+        setReady(true)
+        clearInterval(poll)
+      }
+    }, 100)
+  }, [])
+
   return (
     <div className={css.container}>
       <div className={css.wrapper}>
         <div>
-          <video autoPlay muted playsInline loop className={css.video}>
-            <source src={link?.href} type="video/mp4" />
+          <video
+            autoPlay
+            muted
+            playsInline
+            loop
+            ref={videoRef}
+            style={{ opacity: 0 }}
+            className={`${css.video} ${ready ? css.ready : ''}`}
+          >
+            <source src="/videos/Home/safe-2024-hero-vp9.webm" type="video/webm" />
+            <source src="/videos/Home/safe-2024-hero-hevc.mov" type="video/quicktime; codecs=hvc1" />
           </video>
-          <div className={css.filter} />
         </div>
 
         <Grid container className={css.content} width="100%" height="100%">
