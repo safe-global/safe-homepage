253283ef9106bc5af525bbe88a95bbe36011fa16
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-04-21 00:09:35 +0200

    styles: address designers and QA review  (#324)
    * replace security logos for higher res
    
    * minor CSS fixes after design review

--- src/components/Home/TextBlockImageManifesto/index.tsx
+++ src/components/Home/TextBlockImageManifesto/index.tsx
@@ -1,11 +1,14 @@
 import ManifestoElement from '@/components/Home/TextBlockImageManifesto/ManifestoElement'
 import TextBlockImage, { type TextBlockImageProps } from '@/components/common/TextBlockImage'
+import { Box } from '@mui/material'
 
 const TextBlockImageManifesto = (props: TextBlockImageProps) => {
   return (
-    <TextBlockImage {...props}>
-      <ManifestoElement />
-    </TextBlockImage>
+    <Box mt={{ xs: '-30px', md: 'auto' }}>
+      <TextBlockImage {...props}>
+        <ManifestoElement />
+      </TextBlockImage>
+    </Box>
   )
 }
 
