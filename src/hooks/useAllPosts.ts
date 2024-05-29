5169ae7df936db6770d1c5ef9ea1c870c36fc76d
Diogo Soares <diogo.lsoares@gmail.com>
2024-05-29 16:21:51 +0200

    improve FIXME description

--- src/hooks/useAllPosts.ts
+++ src/hooks/useAllPosts.ts
@@ -3,7 +3,7 @@ import client from '@/lib/contentful'
 import { type EntryCollection } from 'contentful'
 import { useEffect, useState } from 'react'
 
-// FIXME: This hook should make use of useSWR instead of useState
+// FIXME: This hook should make use of useSWR instead of useState but encountered issues with comparing the fallback data and fetched data
 export const useAllPosts = (fallbackData: EntryCollection<TypePostSkeleton, undefined, string>) => {
   const [localAllPosts, setLocalAllPosts] = useState<EntryCollection<TypePostSkeleton, undefined, string>>(fallbackData)
 
