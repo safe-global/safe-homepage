a0ca22cee96f7951996b2e81b13f67a4bbee4c7d
Diogo Soares <diogo.lsoares@gmail.com>
2024-05-29 13:48:49 +0200

    include FIXME comment

--- src/hooks/useAllPosts.ts
+++ src/hooks/useAllPosts.ts
@@ -3,6 +3,7 @@ import client from '@/lib/contentful'
 import { type EntryCollection } from 'contentful'
 import { useEffect, useState } from 'react'
 
+// FIXME: This hook should make use of useSWR instead of useState
 export const useAllPosts = (fallbackData: EntryCollection<TypePostSkeleton, undefined, string>) => {
   const [localAllPosts, setLocalAllPosts] = useState<EntryCollection<TypePostSkeleton, undefined, string>>(fallbackData)
 
