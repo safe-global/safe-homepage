5d4b2b4e6cd861ec19dfb0fd6f8ba67d9fa70b99
Diogo Soares <diogo.lsoares@gmail.com>
2024-06-27 16:54:31 +0200

    styles: mobile responsiveness

--- src/components/DataRoom/ExternalLinksGrid/index.tsx
+++ src/components/DataRoom/ExternalLinksGrid/index.tsx
@@ -1,19 +1,20 @@
 import { Grid, Typography } from '@mui/material'
 import ExternalLinkCard from '@/components/common/Cards/ExternalLinkCard'
 import { type BaseBlock } from '@/components/Home/types'
+import css from './styles.module.css'
 
 const ExternalLinksGrid = ({ title, items }: Pick<BaseBlock, 'title' | 'items'>) => (
-  <Grid container>
-    <Grid item sm={4}>
+  <Grid container className={css.container}>
+    <Grid item md={4} className={css.title}>
       <Typography variant="h3">{title}</Typography>
     </Grid>
 
-    <Grid container item sm={8} columnSpacing={4}>
+    <Grid container item xs={12} md={8} columnSpacing={4} rowSpacing={3}>
       {items?.map((item) => {
         const { title = '', image, link } = item
 
         return (
-          <Grid item sm={6} key={item.link?.href}>
+          <Grid item xs={12} sm={6} key={item.link?.href}>
             <ExternalLinkCard title={title} image={image} link={link} />
           </Grid>
         )
