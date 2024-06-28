520f83ff5bf5898c28afdeb40549867526480ccd
Diogo Soares <diogo.lsoares@gmail.com>
2024-06-28 17:44:11 +0200

    add Social icons

--- src/components/DataRoom/LinksWrapper/index.tsx
+++ src/components/DataRoom/LinksWrapper/index.tsx
@@ -1,13 +1,17 @@
+import Socials from '@/components/Blog/Socials'
 import LinkButton from '@/components/common/LinkButton'
 import SafeLink from '@/components/common/SafeLink'
 import type { Link } from '@/components/Home/types'
+import css from './styles.module.css'
 
+// TODO: define the content to display in the Socials component buttons
 const LinksWrapper = ({ title, href }: Link) => {
   return (
-    <div>
+    <div className={css.wrapper}>
       <SafeLink href={href}>
         <LinkButton>{title}</LinkButton>
       </SafeLink>
+      <Socials authors={[]} title="" />
     </div>
   )
 }
