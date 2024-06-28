ebca5f675fa251c6c3f75a9ce45d0c2b335b0292
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-06-28 17:46:45 +0200

    feat: Links component (#391)
    * feat: LinksWrapper - Dune dashboard
    
    * add Social icons

--- src/components/DataRoom/WorldGDP/index.tsx
+++ src/components/DataRoom/WorldGDP/index.tsx
@@ -1,12 +1,15 @@
 import { Box, Container, Typography } from '@mui/material'
 import type { BaseBlock } from '@/components/Home/types'
+import LinksWrapper from '@/components/DataRoom/LinksWrapper'
 
-const WorldGDP = ({ title }: BaseBlock) => (
+const WorldGDP = ({ title, link }: BaseBlock) => (
   <Container>
     <Box height="100vh" display="flex" flexDirection="column" alignItems="center" justifyContent="center">
       <Typography variant="h1" textAlign="center">
         {title}
       </Typography>
+
+      {link && <LinksWrapper {...link} />}
     </Box>
   </Container>
 )
