cb18b368b56aa901b84d59dd7ed3c518c620e1e7
Diogo Soares <diogo.lsoares@gmail.com>
2024-06-28 16:53:58 +0200

    feat: move DataRoom content to JSON file

--- src/components/DataRoom/DuneBoards/index.tsx
+++ src/components/DataRoom/DuneBoards/index.tsx
@@ -1,37 +1,6 @@
 import ExternalLinksGrid from '@/components/DataRoom/ExternalLinksGrid'
-import { type BaseBlock } from '@/components/Home/types'
+import type { BaseBlock } from '@/components/Home/types'
 
-const data: BaseBlock = {
-  title: 'Dune Boards',
-  text: 'dummy',
-  items: [
-    {
-      title: 'Safe on All chains',
-      image: {
-        src: '/images/dune-icon-name-logo.png',
-        alt: 'Dune Analytics logo',
-      },
-      link: {
-        href: 'https://dune.com/safe/all',
-      },
-    },
-    {
-      title: 'Smart account landscape',
-      image: {
-        src: '/images/dune-icon-name-logo.png',
-        alt: 'Dune Analytics logo',
-      },
-      link: {
-        href: 'https://dune.com/safe/smart-accounts',
-      },
-    },
-  ],
-}
-
-const DuneBoards = () => {
-  const { title, items } = data
-
-  return <ExternalLinksGrid title={title} items={items} />
-}
+const DuneBoards = ({ title, items }: BaseBlock) => <ExternalLinksGrid title={title} items={items} />
 
 export default DuneBoards
