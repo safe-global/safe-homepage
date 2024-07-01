4192434d53d3491363a4321644b49f9c45305c90
Malay Vasa <malayvasa@gmail.com>
2024-07-01 23:02:08 +0530

    refactor: 💡 update based on PR comments

--- src/components/DataRoom/USDCStorage/MatterCanvas.jsx
+++ src/components/DataRoom/USDCStorage/MatterCanvas.jsx
@@ -1,26 +1,97 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Matter from 'matter-js'
+import { debounce } from 'lodash'
 
-const { Bodies, Engine, Events, Mouse, MouseConstraint, Render, Runner, World } = Matter
+const { Bodies, Engine, Render, Runner, World } = Matter
 
-export default function MatterCanvas({ spawnCoins, imgUrl }) {
+const WALL_BORDER_WIDTH = 25
+const WALL_LENGTH = 500
+const COIN_RADIUS = 50
+
+// Scale Determined By Dividing
+// Coin Diameter By Texture Size
+// 100px/256px = 0.390625
+const IMG_SCALE = (COIN_RADIUS * 2) / 256
+
+export default function MatterCanvas({ imgUrl, scrollYProgress }) {
   const canvas = useRef(null)
   const world = useRef(null)
   const engineRef = useRef(null)
   const runnerRef = useRef(null)
-  const [objectsCount, objectsCountSet] = useState(0)
-  const [fps, fpsSet] = useState(0)
-  const [dimensions, setDimensions] = useState({ width: 1000, height: 1000 })
+  const [spawnCoins, setSpawnCoins] = useState(false)
+  const [dimensions, setDimensions] = useState({ width: 500, height: 500 })
 
-  useEffect(() => {
-    const debounce = (func, wait) => {
-      let timeout
-      return (...args) => {
-        clearTimeout(timeout)
-        timeout = setTimeout(() => func.apply(this, args), wait)
-      }
+  const createWorld = useCallback(() => {
+    const engine = Engine.create()
+    engineRef.current = engine
+    world.current = engine.world
+
+    const render = Render.create({
+      canvas: canvas.current,
+      engine,
+      options: {
+        width: dimensions.width,
+        height: dimensions.height,
+        background: '#121312',
+        wireframes: false,
+      },
+    })
+
+    const walls = [
+      { x: 0, y: -WALL_BORDER_WIDTH / 2, width: WALL_LENGTH * 8, height: WALL_BORDER_WIDTH },
+      {
+        x: 0,
+        y: dimensions.height + WALL_BORDER_WIDTH / 2 - 15,
+        width: dimensions.width * 9,
+        height: WALL_BORDER_WIDTH,
+      },
+      { x: 0, y: dimensions.height / 2, width: WALL_BORDER_WIDTH, height: WALL_LENGTH * 8 },
+      { x: dimensions.width, y: dimensions.height / 2, width: WALL_BORDER_WIDTH, height: WALL_LENGTH * 8 },
+    ]
+
+    World.add(
+      engine.world,
+      walls.map((wall) =>
+        Bodies.rectangle(wall.x, wall.y, wall.width, wall.height, {
+          isStatic: true,
+          render: { visible: false },
+        }),
+      ),
+    )
+
+    if (spawnCoins) {
+      const spawnInterval = setInterval(() => {
+        if (engine?.world.bodies.length >= 20) {
+          clearInterval(spawnInterval)
+          return
+        }
+        World.add(engine.world, createCoin())
+      }, 250)
     }
 
+    function createCoin() {
+      return Bodies.circle(Math.random() * dimensions.width * 0.5 + dimensions.width * 0.25, 0, COIN_RADIUS, {
+        restitution: 0.4,
+        render: {
+          sprite: {
+            texture: imgUrl,
+            xScale: IMG_SCALE,
+            yScale: IMG_SCALE,
+          },
+        },
+      })
+    }
+
+    Render.run(render)
+    runnerRef.current = Runner.run(Runner.create(), engine)
+
+    window.Attr = Matter
+    window.engine = engine
+    window.runner = runnerRef.current
+  }, [dimensions.width, dimensions.height, imgUrl, spawnCoins])
+
+  //useEffect To Restart Canvas On Resize
+  useEffect(() => {
     const handleResize = debounce(() => {
       setDimensions({ width: window.innerWidth / 2, height: window.innerHeight })
     }, 300)
@@ -33,135 +104,31 @@ export default function MatterCanvas({ spawnCoins, imgUrl }) {
     }
   }, [])
 
+  //useEffect To Start/Restart Canvas When Scroll Into View
   useEffect(() => {
-    function createWorld() {
-      const engine = Engine.create()
-      engineRef.current = engine
-      world.current = engine.world
+    if (!scrollYProgress) return
+    const handleScrollChange = () => {
+      const latest = scrollYProgress.get()
+      setSpawnCoins(latest > 0.05)
+    }
+    const unsubscribe = scrollYProgress.onChange(handleScrollChange)
+    return () => unsubscribe()
+  }, [scrollYProgress])
 
-      console.log('createWorld')
-
-      // create a renderer
-      const render = Render.create({
-        canvas: canvas.current || undefined,
-        engine,
-        options: {
-          width: dimensions.width,
-          height: dimensions.height,
-          background: '#121312',
-          showCollisions: false,
-          showVelocity: false,
-          showAxes: false,
-          wireframes: false,
-        },
-      })
-
-      const WALL_BORDER_WIDTH = 25
-      const WALL_LENGTH = 500
-
-      // walls
-      World.add(engine.world, [
-        Bodies.rectangle(0, 0 - WALL_BORDER_WIDTH / 2, WALL_LENGTH * 8, WALL_BORDER_WIDTH, {
-          isStatic: true,
-          render: { visible: false },
-        }),
-
-        Bodies.rectangle(0, dimensions.height + WALL_BORDER_WIDTH / 2 - 15, dimensions.width * 9, WALL_BORDER_WIDTH, {
-          isStatic: true,
-          render: { visible: false },
-        }),
-
-        Bodies.rectangle(0, dimensions.height - dimensions.height / 2, WALL_BORDER_WIDTH, WALL_LENGTH * 8, {
-          isStatic: true,
-          render: { visible: false },
-        }),
-        Bodies.rectangle(
-          dimensions.width,
-          dimensions.height - dimensions.height / 2,
-          WALL_BORDER_WIDTH,
-          WALL_LENGTH * 8,
-          {
-            isStatic: true,
-            render: { visible: false },
-          },
-        ),
-      ])
-
-      // Generate 20 coins over 5 seconds if spawnCoins is true
-      if (spawnCoins) {
-        let ballsCreated = 0
-        const intervalId = setInterval(() => {
-          if (ballsCreated >= 20) {
-            clearInterval(intervalId)
-            return
-          }
-          createBalls({
-            x: (Math.random() * dimensions.width) / 2 + dimensions.width / 4,
-            y: Math.random() * dimensions.height,
-          })
-          ballsCreated++
-        }, 5000 / 20) // 5 seconds divided by 20 Coins
+  // useEffect To Create And Clean Up The Matter.js World
+  useEffect(() => {
+    const cleanup = () => {
+      if (runnerRef.current) {
+        Runner.stop(runnerRef.current)
+        Engine.clear(engineRef.current)
       }
-
-      Events.on(engine, 'afterUpdate', (ev) => {
-        // const time = engine.timing.timestamp
-        objectsCountSet(ev.source.world.bodies.length)
-
-        ev.source.world.bodies.forEach((b) => {
-          if (b.position.x > dimensions.width || b.position.x < 0 || b.position.y > dimensions.height) {
-            World.remove(engine.world, b)
-          }
-        })
-        fpsSet(Math.abs(runnerRef.current.fps))
-      })
-
-      function createBalls(positionXY) {
-        if (!positionXY) {
-          return
-        }
-
-        World.add(
-          engine.world,
-          Bodies.circle(positionXY.x + Math.random() * 15 || dimensions.width / 2, 0, 50, {
-            restitution: 0.4,
-            render: {
-              sprite: {
-                texture: imgUrl, // Path to your image
-                xScale: 1, // Scale the image if needed
-                yScale: 1,
-              },
-            },
-          }),
-        )
-      }
-
-      Render.run(render)
-
-      // create runner
-      const runner = Runner.create()
-      runnerRef.current = runner
-      // run the engine
-      Runner.run(runner, engine)
-
-      // add To Global
-      window.Matter = Matter
-      window.engine = engine
-      window.runner = runner
     }
 
-    if (runnerRef.current) {
-      Runner.stop(runnerRef.current)
-      Engine.clear(engineRef.current)
-    }
-
+    cleanup()
     createWorld()
 
-    return () => {
-      console.log('clear')
-      Runner.stop(runnerRef.current)
-      Engine.clear(engineRef.current)
-    }
-  }, [canvas, world, dimensions, imgUrl, spawnCoins])
+    return cleanup
+  }, [createWorld])
 
   return (
     <div
--- src/components/DataRoom/USDCStorage/index.tsx
+++ src/components/DataRoom/USDCStorage/index.tsx
@@ -1,94 +1,62 @@
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import type { BaseBlock } from '@/components/Home/types'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import MatterCanvas from './MatterCanvas'
-import { useEffect, useRef, useState } from 'react'
+import { useRef } from 'react'
 import LinksWrapper from '../LinksWrapper'
 import css from './styles.module.css'
+import MotionTypography from './MotionTypography'
 
-const USDCStorage = ({ title, text, link }: BaseBlock) => {
-  const targetRef = useRef<HTMLDivElement>(null)
-  const [spawnCoins, setCoins] = useState(false)
+const USDC_PERCENTAGE = '10%'
 
-  // Define Animation Variants For Content
-  const variants = {
-    initial: {
-      opacity: 0,
-      y: 50,
-    },
-    animate: (custom: number) => ({
-      opacity: 1,
-      y: 0,
-      transition: { delay: custom },
-    }),
-  }
+const USDCStorage = ({ title, text, link, image }: BaseBlock) => {
+  const containerRef = useRef<HTMLDivElement>(null)
 
   // Initialize Tracking scrollYProgress
   const { scrollYProgress } = useScroll({
-    target: targetRef,
+    target: containerRef,
     offset: ['start end', 'end start'],
   })
 
   // Map Container Scroll To Sticky Section Scroll
-  const MAP_Y_PROGRESS = useTransform(scrollYProgress, [0.25, 0.75], [0, 1])
+  const mapYProgress = useTransform(scrollYProgress, [0.25, 0.75], [0, 1])
 
   // Transform Properties
-  const X_TRANSFORM_CONTENT = useTransform(MAP_Y_PROGRESS, [0.8, 1], ['0%', '-100%'])
-  const X_TRANSFORM_CANVAS = useTransform(MAP_Y_PROGRESS, [0.8, 1], ['0%', '100%'])
-
-  const COINS_IMG_URL = '/images/Dataroom/USDC.png'
-
-  //Control USDC Coins Spawn With Scroll
-  useEffect(() => {
-    const unsubscribe = scrollYProgress.onChange((latest) => {
-      if (latest > 0.05) {
-        setCoins(true)
-      } else if (latest < 0.05) {
-        setCoins(false)
-      }
-    })
-    return () => unsubscribe()
-  }, [scrollYProgress])
+  const xTransformContent = useTransform(mapYProgress, [0.8, 1], ['0%', '-100%'])
+  const xTransformCanvas = useTransform(mapYProgress, [0.8, 1], ['0%', '100%'])
 
   return (
-    <Box ref={targetRef} className={css.sectionContainer}>
-      <Box className={css.stickyContainer}>
+    <div ref={containerRef} className={css.sectionContainer}>
+      <div className={css.stickyContainer}>
         <motion.div
           style={{
-            translateX: X_TRANSFORM_CONTENT,
+            translateX: xTransformContent,
           }}
           className={css.content}
         >
-          <motion.div
-            variants={variants}
-            initial="initial"
-            custom={0.5}
-            whileInView="animate"
-            viewport={{ once: true }}
-          >
-            <Typography variant="h1">{title}</Typography>
-          </motion.div>
-          <motion.div
-            variants={variants}
-            initial="initial"
-            custom={1.5}
-            whileInView="animate"
-            viewport={{ once: true }}
-          >
-            <Typography variant="h1">{text}</Typography>
-          </motion.div>
-          {link && <LinksWrapper {...link} />}
+          <MotionTypography customDelay={0.5}>
+            <Typography variant="h2">{title}</Typography>
+          </MotionTypography>
+          <MotionTypography customDelay={1.5}>
+            <Typography className={css.usdcPercentage}>
+              <b>{USDC_PERCENTAGE}</b>
+            </Typography>
+            <Typography variant="h2" className={css.text}>
+              {text}
+            </Typography>
+            {link && <LinksWrapper {...link} />}
+          </MotionTypography>
         </motion.div>
         <motion.div
           className={css.canvas}
           style={{
-            translateX: X_TRANSFORM_CANVAS,
+            translateX: xTransformCanvas,
           }}
         >
-          <MatterCanvas spawnCoins={spawnCoins} imgUrl={COINS_IMG_URL} />
+          <MatterCanvas scrollYProgress={scrollYProgress} imgUrl={image?.src} />
         </motion.div>
-      </Box>
-    </Box>
+      </div>
+    </div>
   )
 }
 
