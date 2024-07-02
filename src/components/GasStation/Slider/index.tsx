727e620015896bf2d51879d919dffdea2cb17924
Diogo Soares <diogo.lsoares@gmail.com>
2024-07-03 00:01:37 +0200

    fix: separate animation and slider classes

--- src/components/GasStation/Slider/index.tsx
+++ src/components/GasStation/Slider/index.tsx
@@ -6,12 +6,14 @@ const SLIDER_ITEMS = 7
 
 const Slider = ({ text }: BaseBlock) => (
   <div className={css.wrapper}>
-    <div className={css.animationSlider}>
-      {Array.from({ length: SLIDER_ITEMS }).map((_, idx) => (
-        <Typography variant="caption" className={css.item} key={idx}>
-          {text}
-        </Typography>
-      ))}
+    <div className={css.animation}>
+      <div className={css.slider}>
+        {Array.from({ length: SLIDER_ITEMS }).map((_, idx) => (
+          <Typography variant="caption" className={css.item} key={idx}>
+            {text}
+          </Typography>
+        ))}
+      </div>
     </div>
   </div>
 )
