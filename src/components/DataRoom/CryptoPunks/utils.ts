652cc36d764a09ac0dc4eaf7666f4ca4430c1230
Malay Vasa <malayvasa@gmail.com>
2024-07-03 19:53:43 +0530

    feat: 🎸 add mobile support for cryptopunks component

--- src/components/DataRoom/CryptoPunks/utils.ts
+++ src/components/DataRoom/CryptoPunks/utils.ts
@@ -1,7 +1,7 @@
 export function getColor() {
   const chance = Math.random()
 
-  if (chance <= 0.14) return '#12FF80' // Green (14% CHANCE)
+  if (chance <= 0.24) return '#12FF80' // Green (14% CHANCE)
   if (chance <= 0.17) return '#12A154' // Dark Green (3% CHANCE)
   if (chance <= 0.2) return '#124228' // Darker Green (3% CHANCE)
 
