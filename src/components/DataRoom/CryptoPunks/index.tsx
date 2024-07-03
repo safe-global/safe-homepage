1aad8fa1e748fc91c297f0cfa647e5b42c63e6b4
Malay Vasa <malayvasa@gmail.com>
2024-07-03 21:34:51 +0530

    fix: 🐛 update breakpoint

--- src/components/DataRoom/CryptoPunks/index.tsx
+++ src/components/DataRoom/CryptoPunks/index.tsx
@@ -18,7 +18,7 @@ const CRYPTOPUNK_COLUMNS_NR = 24
 
 const CryptoPunks = ({ title, text, link }: BaseBlock) => {
   const backgroundRef = useRef<HTMLDivElement>(null)
-  const isMobile = useMediaQuery('(max-width:767px)')
+  const isMobile = useMediaQuery('(max-width:768px)')
   const { scrollYProgress } = useScroll({
     target: backgroundRef,
     offset: ['start end', 'end start'],
