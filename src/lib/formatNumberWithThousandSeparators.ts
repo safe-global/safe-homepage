bd9a3eff6d9c8103fb691333408d48c37d4259c3
Diogo Soares <diogo.lsoares@gmail.com>
2024-07-07 23:06:08 +0200

    feat: use Dune pulled value for Total Volume Transferred section

---
+++ src/lib/formatNumberWithThousandSeparators.ts
@@ -0,0 +1,14 @@
+/**
+ * Formats a number by adding thousand separators.
+ *
+ * This function takes a number, discards its decimal part, and then formats it
+ * according to the 'en-US' locale, which adds thousand separators to the integer part of the number.
+ *
+ * @param {number} num - The number to format.
+ * @returns {string} The formatted number with thousand separators.
+ */
+export const formatNumberWithThousandSeparators = (num: number): string => {
+  const integerPart = Math.floor(num) // Discard the decimal part
+
+  return new Intl.NumberFormat('en-US').format(integerPart)
+}
