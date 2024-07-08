a17385a6c1407393ada16463ccbc89d6aa076afb
Diogo Soares <diogo.lsoares@gmail.com>
2024-07-08 09:45:20 +0200

    refactor: delete redundant component

--- src/components/DataRoom/ExternalReports/index.tsx
+++
@@ -1,6 +0,0 @@
-import ExternalLinksGrid from '@/components/DataRoom/ExternalLinksGrid'
-import type { BaseBlock } from '@/components/Home/types'
-
-const ExternalReports = ({ title, items }: BaseBlock) => <ExternalLinksGrid title={title} items={items} />
-
-export default ExternalReports
