08ab05ddffec5aea53b17977e8d93322343c05b8
Diogo Soares <diogo.lsoares@gmail.com>
2024-07-10 14:37:15 +0200

    fix: the error handling in fetching from Dune

--- src/hooks/useSafeDataRoomStats.ts
+++ src/hooks/useSafeDataRoomStats.ts
@@ -26,16 +26,17 @@ type DuneDataRoomStats = {
 const QUERY_ID_DATAROOM_STATS = 3864414
 
 export const fetchDataRoomStats = async (): Promise<DuneDataRoomStats | null> => {
-  return fetch(duneQueryUrlBuilder(QUERY_ID_DATAROOM_STATS, DUNE_API_KEY))
-    .then((res) => {
-      if (res.ok) {
+  return (
+    fetch(duneQueryUrlBuilder(QUERY_ID_DATAROOM_STATS, DUNE_API_KEY))
+      .then((res) => {
+        if (!res.ok) throw new Error('Response error')
+
         return res.json()
-      } else {
-        throw new Error('Response error')
       })
-    // Validate response instead of casting
-    .then((data) => data.result.rows[0] as DuneDataRoomStats)
-    .catch(() => null)
+      // Validate response instead of casting
+      .then((data) => data.result.rows[0] as DuneDataRoomStats)
+      .catch(() => null)
+  )
 }
 
 export const useSafeDataRoomStats = (): Record<string, number | null> => {
