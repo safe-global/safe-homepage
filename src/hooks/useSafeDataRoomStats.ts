f9d6834f23f372538517f46f4d0833f94cba34cb
Diogo Soares <diogo.lsoares@gmail.com>
2024-07-10 17:34:20 +0200

    remove comment

--- src/hooks/useSafeDataRoomStats.ts
+++ src/hooks/useSafeDataRoomStats.ts
@@ -26,17 +26,14 @@ type DuneDataRoomStats = {
 const QUERY_ID_DATAROOM_STATS = 3864414
 
 export const fetchDataRoomStats = async (): Promise<DuneDataRoomStats | null> => {
-  return (
-    fetch(duneQueryUrlBuilder(QUERY_ID_DATAROOM_STATS, DUNE_API_KEY))
-      .then((res) => {
-        if (!res.ok) throw new Error('Response error')
+  return fetch(duneQueryUrlBuilder(QUERY_ID_DATAROOM_STATS, DUNE_API_KEY))
+    .then((res) => {
+      if (!res.ok) throw new Error('Response error')
 
-        return res.json()
-      })
-      // Validate response instead of casting
-      .then((data) => data.result.rows[0] as DuneDataRoomStats)
-      .catch(() => null)
-  )
+      return res.json()
+    })
+    .then((data) => data.result.rows[0] as DuneDataRoomStats)
+    .catch(() => null)
 }
 
 export const useSafeDataRoomStats = (): Record<string, number | null> => {
