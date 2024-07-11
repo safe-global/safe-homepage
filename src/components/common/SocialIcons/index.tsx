30c814b39dea0e08d554550c7c3c41bc6f23e94c
Diogo Soares <diogo.lsoares@gmail.com>
2024-07-11 14:56:22 +0200

    refactor: extract social icons to a component

---
+++ src/components/common/SocialIcons/index.tsx
@@ -0,0 +1,20 @@
+import { IconButton, SvgIcon } from '@mui/material'
+import css from './styles.module.css'
+
+export type SocialsIconProps = {
+  label: string
+  url: string
+  icon: any
+}
+
+const SocialIcons = ({ items }: { items: Array<SocialsIconProps> }) => (
+  <div className={css.socials}>
+    {items.map(({ label, url, icon }, index) => (
+      <IconButton aria-label={`Share on ${label}`} href={url} target="_blank" rel="noreferrer" size="small" key={index}>
+        <SvgIcon component={icon} fontSize="inherit" color="inherit" inheritViewBox />
+      </IconButton>
+    ))}
+  </div>
+)
+
+export default SocialIcons
