2c029947e1a9929498781e1771265116f198ca5e
Malay Vasa <malayvasa@gmail.com>
2024-07-12 15:28:31 +0530

    feat: 🎸 integrate live dune data

--- src/components/DataRoom/WorldGDP/index.tsx
+++ src/components/DataRoom/WorldGDP/index.tsx
@@ -3,31 +3,51 @@ import type { BaseBlock } from '@/components/Home/types'
 import LinksWrapper from '@/components/DataRoom/LinksWrapper'
 import css from './styles.module.css'
 import MotionContainer from './MotionContainer'
+import { useSafeDataRoomStats } from '@/hooks/useSafeDataRoomStats'
+import { formatCurrency } from '@/lib/formatCurrency'
 
-const GDP_VALUE = '$100B+'
+type ExtendedBaseBlock = BaseBlock & {
+  subtitle: string
+}
 
-const WorldGDP = ({ title, text, link }: BaseBlock) => (
-  <div className={css.sectionContainer}>
-    <div className={css.stickyContainer}>
-      <Typography className={css.text}>{text}</Typography>
-      <div className={css.contentContainer}>
-        <MotionContainer customDelay={0.2}>
-          <div>
-            <video className={css.video} src="/videos/DataRoom/SafeGlobe.mp4" autoPlay muted loop />
-          </div>
-        </MotionContainer>
-        <MotionContainer customDelay={0.5}>
-          <Typography className={css.title}>{title}</Typography>
-        </MotionContainer>
-        <MotionContainer customDelay={0.7}>
-          <Typography variant="h1">
-            <b>{GDP_VALUE}</b>
-          </Typography>
-        </MotionContainer>
+const VALUE_LOCKED_FALLBACK = 10000000000
+const PERCENTAGE_FALLBACK = 0.001
+
+const WorldGDP = ({ title, subtitle, text, link }: ExtendedBaseBlock) => {
+  const { tvlToGDPPercentage, totalValueLocked } = useSafeDataRoomStats()
+  const valueLocked = totalValueLocked || VALUE_LOCKED_FALLBACK
+  const percentageValue = tvlToGDPPercentage || PERCENTAGE_FALLBACK
+
+  const displayTVLValue = formatCurrency(valueLocked, 0)
+  const percentageToDisplay = '~' + (percentageValue * 100).toFixed(2) + '%'
+
+  return (
+    <div className={css.sectionContainer}>
+      <div className={css.stickyContainer}>
+        <Typography className={css.text}>{text}</Typography>
+        <div className={css.contentContainer}>
+          <MotionContainer customDelay={0.2}>
+            <div>
+              <video className={css.video} src="/videos/DataRoom/SafeGlobe.mp4" autoPlay muted loop />
+            </div>
+          </MotionContainer>
+          <MotionContainer customDelay={0.5}>
+            <div className={css.percentContainer}>
+              <Typography className={css.title}>{title}</Typography>
+              <Typography className={css.title}>{percentageToDisplay}</Typography>
+            </div>
+            <Typography className={css.title}>{subtitle}</Typography>
+          </MotionContainer>
+          <MotionContainer customDelay={0.7}>
+            <Typography variant="h1">
+              <b>{displayTVLValue}</b>
+            </Typography>
+          </MotionContainer>
+        </div>
+        <div className={css.linksContainer}>{link && <LinksWrapper {...link} />}</div>
       </div>
-      <div className={css.linksContainer}>{link && <LinksWrapper {...link} />}</div>
     </div>
-  </div>
-)
+  )
+}
 
 export default WorldGDP
