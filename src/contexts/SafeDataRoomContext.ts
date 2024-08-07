2debc7a035cfa817799e40b574eb6feac4820ea4
Malay Vasa <malayvasa@gmail.com>
2024-08-07 13:32:36 +0530

    refactor: 💡 remove duplicate safe tvl data props

--- src/contexts/SafeDataRoomContext.ts
+++ src/contexts/SafeDataRoomContext.ts
@@ -1,7 +1,6 @@
 import { createContext } from 'react'
 
 type SafeDataRoomStats = {
-  totalValueLocked: number | null
   tvlToGDPPercentage: number | null
   usdcPercentageStored: number | null
   cryptoPunksStoredPercentage: number | null
@@ -15,7 +14,6 @@ type SafeDataRoomStats = {
 }
 
 const SafeDataRoomContext = createContext<SafeDataRoomStats>({
-  totalValueLocked: null,
   tvlToGDPPercentage: null,
   usdcPercentageStored: null,
   cryptoPunksStoredPercentage: null,
