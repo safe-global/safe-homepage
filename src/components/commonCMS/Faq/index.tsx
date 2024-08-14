3b8bd501ccbe8b48bf8589b9d5cd3f75c37c5895
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-08-14 10:21:06 +0200

    feat(foundry): TextCenteredBlock & FAQ sections (#437)
    * feat: add CenteredTextBlock
    
    * feat: FAQ section
    
    * styles: clean up classes on the Faq component
    
    * Update src/components/commonCMS/Faq/index.tsx
    
    extract open accordion value to a variable
    
    Co-authored-by: Aaron Cook <aaron@safe.global>
    
    ---------
    
    Co-authored-by: Aaron Cook <aaron@safe.global>

---
+++ src/components/commonCMS/Faq/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import {
+  Accordion,
+  AccordionDetails,
+  type AccordionProps,
+  AccordionSummary,
+  Container,
+  Grid,
+  Typography,
+} from '@mui/material'
+import PlusIcon from '@/public/images/plus-sign-icon.svg'
+import MinusIcon from '@/public/images/minus-sign-icon.svg'
+import { isEntryTypeBaseBlock } from '@/lib/typeGuards'
+import RichText from '@/components/common/RichText'
+import { type BaseBlockEntry } from '@/config/types'
+import layoutCss from '@/components/common/styles.module.css'
+import css from './styles.module.css'
+
+const Faq = (props: BaseBlockEntry) => {
+  const { title, items } = props.fields
+
+  // Tracks which accordion is open
+  const [openMap, setOpenMap] = useState<Record<number, boolean>>()
+
+  const itemsList = items?.filter(isEntryTypeBaseBlock) ?? []
+
+  return (
+    <Container className={layoutCss.container}>
+      <Grid container className={css.gridContainer}>
+        <div className={css.spot} />
+
+        <Grid item md={5} width="100%">
+          <RichText {...title} />
+        </Grid>
+
+        <Grid item md={7}>
+          {itemsList.map((item, index) => {
+            const { title, text } = item.fields
+
+            const handleChange: AccordionProps['onChange'] = (_, expanded) => {
+              setOpenMap((prev) => ({
+                ...prev,
+                [index]: expanded,
+              }))
+            }
+            const expanded = openMap?.[index] ?? false
+
+            return (
+              <Accordion
+                className={css.accordion}
+                expanded={expanded}
+                onChange={handleChange}
+                key={index}
+                disableGutters
+                square
+              >
+                <AccordionSummary
+                  expandIcon={expanded ? <MinusIcon /> : <PlusIcon />}
+                  onClick={() => {
+                    !expanded
+                  }}
+                >
+                  <Typography variant="h4">
+                    <RichText {...title} />
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails className={css.details}>{text && <RichText {...text} />}</AccordionDetails>
+              </Accordion>
+            )
+          })}
+        </Grid>
+      </Grid>
+    </Container>
+  )
+}
+
+export default Faq
