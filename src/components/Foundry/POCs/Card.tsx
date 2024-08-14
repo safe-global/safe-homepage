6848beb5332351233826b1a5320c8c1d8ba541bb
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-08-14 17:05:25 +0200

    feat: POCs section

---
+++ src/components/Foundry/POCs/Card.tsx
@@ -0,0 +1,55 @@
+import { ButtonBase, Typography } from '@mui/material'
+import RichText from '@/components/common/RichText'
+import ButtonsWrapper from '@/components/Token/ButtonsWrapper'
+import { isAsset, isEntryTypeButton, isEntryTypeExternalURL } from '@/lib/typeGuards'
+import { type BaseBlockEntry } from '@/config/types'
+import GithubIcon from '@/public/images/github-icon.svg'
+import css from './styles.module.css'
+
+const Card = (props: BaseBlockEntry) => {
+  const { caption, title, text, link, image, buttons } = props.fields
+
+  const buttonBaseAttributes = {
+    disableRipple: true,
+    target: '_blank',
+    rel: 'noreferrer',
+  }
+
+  const buttonsList = buttons?.filter(isEntryTypeButton) || []
+  const linkURL = isEntryTypeExternalURL(link) ? link.fields.url : undefined
+
+  return (
+    <div className={css.card}>
+      <div className={css.cardHeader}>
+        <Typography variant="h4">
+          <RichText {...title} />
+        </Typography>
+
+        <ButtonBase {...buttonBaseAttributes} aria-label="Github URL" href={linkURL}>
+          <GithubIcon />
+        </ButtonBase>
+      </div>
+
+      <div className={css.cardBody}>
+        {text ? <RichText {...text} /> : null}
+
+        <div className={css.extraText}>
+          <Typography color="primary.light">{caption}</Typography>
+
+          {isAsset(image) && image.fields.file?.url ? (
+            <div className={css.partner}>
+              <img src={image.fields.file.url} alt={image.fields.title ?? ''} />
+              <span>{image.fields.description}</span>
+            </div>
+          ) : null}
+        </div>
+
+        <div className={css.buttons}>
+          <ButtonsWrapper buttons={buttonsList} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
