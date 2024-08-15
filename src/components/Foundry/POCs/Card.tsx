a5f9c3e26c8ae15c3edc101955e020c78a848cf6
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-08-15 10:06:45 +0200

    feat(foundry): POC table (#440)
    * feat: POCs section
    
    * style: align Card buttons to the left on mobile
    
    * fix: use SafeLink

---
+++ src/components/Foundry/POCs/Card.tsx
@@ -0,0 +1,50 @@
+import { Typography } from '@mui/material'
+import RichText from '@/components/common/RichText'
+import ButtonsWrapper from '@/components/Token/ButtonsWrapper'
+import { isAsset, isEntryTypeButton, isEntryTypeExternalURL } from '@/lib/typeGuards'
+import { type BaseBlockEntry } from '@/config/types'
+import GithubIcon from '@/public/images/github-icon.svg'
+import css from './styles.module.css'
+import SafeLink from '@/components/common/SafeLink'
+
+const Card = (props: BaseBlockEntry) => {
+  const { caption, title, text, link, image, buttons } = props.fields
+
+  const buttonsList = buttons?.filter(isEntryTypeButton) || []
+  const linkURL = isEntryTypeExternalURL(link) ? link.fields.url : ''
+
+  return (
+    <div className={css.card}>
+      <div className={css.cardHeader}>
+        <Typography variant="h4">
+          <RichText {...title} />
+        </Typography>
+
+        <SafeLink href={linkURL}>
+          <GithubIcon />
+        </SafeLink>
+      </div>
+
+      <div className={css.cardBody}>
+        {text ? <RichText {...text} /> : null}
+
+        <div className={css.extraText}>
+          <Typography color="primary.light">{caption}</Typography>
+
+          {isAsset(image) && image.fields.file?.url ? (
+            <div className={css.partner}>
+              <img src={image.fields.file.url} alt={image.fields.title ?? ''} />
+              <span>{image.fields.description}</span>
+            </div>
+          ) : null}
+        </div>
+
+        <div className={css.buttons}>
+          <ButtonsWrapper buttons={buttonsList} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
