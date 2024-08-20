44b8b93c710b316f03bda9587702277883687ea4
Malay Vasa <malayvasa@gmail.com>
2024-08-20 13:44:52 +0530

    fix: 🐛 remove unused variables and imports

--- src/components/DataRoom/SeeMore/SlidingWave.tsx
+++ src/components/DataRoom/SeeMore/SlidingWave.tsx
@@ -1,5 +1,4 @@
 import type { BaseBlock } from '@/components/Home/types'
-import { useIsMediumScreen } from '@/hooks/useMaxWidth'
 import { Typography } from '@mui/material'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import type { RefObject } from 'react'
@@ -12,13 +11,11 @@ export default function SlidingWave({
   text: BaseBlock['text']
   containerRef: RefObject<HTMLDivElement>
 }) {
-  const isMobile = useIsMediumScreen()
   const { scrollYProgress } = useScroll({
     target: containerRef,
     offset: ['start end', 'end start'],
   })
 
-  const opacity = useTransform(scrollYProgress, [0, 0.3, 0.6, 0.75], [0, 1, 1, 0])
   const textOpacity = useTransform(scrollYProgress, [0, 0.3, 0.5, 0.75], [0, 1, 1, 0])
   const translate = useTransform(scrollYProgress, [0, 0.2, 0.55, 0.75], ['-250px', '0px', '0px', '250px'])
 
@@ -27,9 +24,6 @@ export default function SlidingWave({
       <motion.div style={{ opacity: textOpacity, translateY: translate }} className={css.text}>
         <Typography variant="h1">{text}</Typography>
       </motion.div>
-      {
-        //Moved the wave animation to PR #441
-      }
     </>
   )
 }
