cd998428e7faebae91a5f37fb66b0725aee18f66
Malay Vasa <malayvasa@gmail.com>
2024-08-20 15:45:43 +0530

    fix: 🐛 fix text bounce back issue

--- src/components/DataRoom/SeeMore/SlidingContent.tsx
+++ src/components/DataRoom/SeeMore/SlidingContent.tsx
@@ -16,7 +16,7 @@ export default function SlidingContent({
     offset: ['start end', 'end start'],
   })
 
-  const textOpacity = useTransform(scrollYProgress, [0, 0.3, 0.5, 0.75], [0, 1, 1, 0])
+  const textOpacity = useTransform(scrollYProgress, [0, 0.3, 0.5, 0.65], [0, 1, 1, 0])
   const translate = useTransform(scrollYProgress, [0, 0.2, 0.55, 0.75], ['-250px', '0px', '0px', '250px'])
 
   return (
