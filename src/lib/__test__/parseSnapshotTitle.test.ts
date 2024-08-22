0ea71da54ec2b5fdf79d1644d4e03328fc02d0ba
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-08-22 11:32:58 +0200

    fix: parse Snapshot proposal title

--- src/lib/__test__/parseSnapshotTitle.test.ts
+++ src/lib/__test__/parseSnapshotTitle.test.ts
@@ -8,10 +8,12 @@ const realTitles = [
   '[SEP #5] Redistributing Unredeemed Tokens From User Airdrop Allocation',
   '# [SEP #6] Safe Grants Program (SGP)',
   '[SEP #20] [OBRA] Formalizing the Guardian Role onchain with Hats Protocol - Hats Protocol',
+  '[SEP 36]: [OBRA, Strategy 3] - Simulations/Demos and Tidbits for All Safe Use Cases',
+  '[SEP 41] [OBRA] Safe Ecosystem Roundups and Highlights by FinOps3',
+  '[SEP 43][OBRA] ZK Email Account Recovery - ZK Email',
 ]
 
 const badTitles = [
-  'SEP 6 Safe Grants Program (SGP)', // missing # before the number
   'SEP Safe Grants Program (SGP)', // missing number
 ]
 
@@ -36,10 +38,18 @@ describe('parseSnapshotTitle', () => {
       '20',
       '[OBRA] Formalizing the Guardian Role onchain with Hats Protocol - Hats Protocol',
     ])
+    expect(parseSnapshotTitle(realTitles[7])).toEqual([
+      '36',
+      '[OBRA, Strategy 3] - Simulations/Demos and Tidbits for All Safe Use Cases',
+    ])
+    expect(parseSnapshotTitle(realTitles[8])).toEqual([
+      '41',
+      '[OBRA] Safe Ecosystem Roundups and Highlights by FinOps3',
+    ])
+    expect(parseSnapshotTitle(realTitles[9])).toEqual(['43', '[OBRA] ZK Email Account Recovery - ZK Email'])
   })
 
   it('returns 0 and the original title if the both groups are not present in the title', () => {
     expect(parseSnapshotTitle(badTitles[0])).toEqual(['0', badTitles[0]])
-    expect(parseSnapshotTitle(badTitles[1])).toEqual(['0', badTitles[1]])
   })
 })
