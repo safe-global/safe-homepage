8f611293edf441443c07f678e46ccb389a8635e9
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-08-23 14:01:20 +0200

    feat: add missing directives to load the content and scripts in the remaining pages

--- src/config/securityHeaders.ts
+++ src/config/securityHeaders.ts
@@ -2,14 +2,16 @@ import { IS_PRODUCTION } from '@/config/constants'
 
 export const ContentSecurityPolicy = `
  default-src 'self';
- connect-src 'self' https://api.ashbyhq.com/posting-api/job-board/safe.global/;
+ connect-src 'self' https://api.ashbyhq.com/posting-api/job-board/safe.global/ https://ecosystem-database.staging.5afe.dev/data.json https://hub.snapshot.org/graphql https://cdn.contentful.com/spaces/1i5gc724wjeu/ https://metrics.hotjar.io/;
  script-src 'self' ${
    IS_PRODUCTION ? '' : "'unsafe-eval'"
- } 'unsafe-inline' https://static.hotjar.com/c/hotjar-3603830.js;
+ } 'unsafe-inline' https://static.hotjar.com/c/hotjar-3603830.js https://www.googletagmanager.com;
  style-src 'self' 'unsafe-inline';
- font-src 'self' data:;
+ font-src 'self';
  object-src 'none';
  base-uri 'none';
+ img-src 'self' http://images.ctfassets.net/ https://ecosystem-database.staging.5afe.dev/logos/ https://safe-claiming-app-data.safe.global/guardians/images/ data:;
+ frame-src https://safe.mirror.xyz/ https://www.youtube-nocookie.com/ https://cdn.jwplayer.com/;
 `
   .replace(/\s{2,}/g, ' ')
   .trim()
