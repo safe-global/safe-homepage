443d1a4972a402706ad7d5fda18665ce9541d453
Malay Vasa <malayvasa@gmail.com>
2024-09-11 19:05:10 +0530

    fix: 🐛 address world gdp section qa feedback

--- src/components/DataRoom/WorldGDP/index.tsx
+++ src/components/DataRoom/WorldGDP/index.tsx
@@ -2,16 +2,16 @@ import type { BaseBlock } from '@/components/Home/types'
 import LinksWrapper from '@/components/DataRoom/LinksWrapper'
 import css from './styles.module.css'
 import dynamic from 'next/dynamic'
+import { Typography } from '@mui/material'
 
 const SlidingVideo = dynamic(() => import('./SlidingVideo'))
 const SlidingText = dynamic(() => import('./SlidingText'))
 
-const WorldGDP = ({ title, link }: BaseBlock) => {
+const WorldGDP = ({ text, title, link }: BaseBlock) => {
   return (
     <div className={css.sectionContainer}>
       <div className={css.stickyContainer}>
-        {/* This element is commented out as we are waiting on the final copy*/}
-        {/* <Typography className={css.text}>{text}</Typography> */}
+        <Typography className={css.text}>{text}</Typography>
 
         <div className={css.contentContainer}>
           <SlidingVideo />
