ab0f131f19c4ffba912dea6ea34cb40420213369
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-09-20 15:03:26 +0200

    fix: WorldGDP video plays inline (#464)
    * fix: WorldGDP video plays inline
    
    * style: add bouncing animation to scroll indicator
    
    * fix: do not allow the WorldGDP text container spill into the next section

--- src/components/DataRoom/WorldGDP/SlidingVideo.tsx
+++ src/components/DataRoom/WorldGDP/SlidingVideo.tsx
@@ -3,7 +3,7 @@ import css from './styles.module.css'
 
 const SlidingVideo = () => (
   <MotionTypography animateYFrom={-30} customDelay={0.2}>
-    <video className={css.video} src="/videos/DataRoom/SafeGlobe.mp4" autoPlay muted loop />
+    <video className={css.video} src="/videos/DataRoom/SafeGlobe.mp4" autoPlay playsInline muted loop />
   </MotionTypography>
 )
 
