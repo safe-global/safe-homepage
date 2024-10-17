6eae2de50244618ba2e1ea23c15ceeac640bdac3
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-10-17 10:59:34 +0100

    feat: update page meta tags

--- src/components/Alpha/Header/index.tsx
+++ src/components/Alpha/Header/index.tsx
@@ -3,12 +3,12 @@ import css from './styles.module.css'
 const HeaderLetter = () => (
   <div className={css.header}>
     <div className={css.telegramWrapper}>
-      <a href="https://t.me/safechat" target="_blank" rel="noreferrer" className={css.telegramLink}>
+      <a href="" target="_blank" rel="noreferrer" className={css.telegramLink}>
         Join
         <img src="/images/telegram-logo.svg" alt="Telegram logo" />
       </a>
 
-      <p>{`to get Safe{Alpha}`}</p>
+      <div>{`to get Safe{Alpha}`}</div>
     </div>
   </div>
 )
--- src/components/Alpha/index.tsx
+++ src/components/Alpha/index.tsx
@@ -1,7 +1,19 @@
 import type { ReactElement } from 'react'
 import PageContent from '@/components/common/PageContent'
 import alphaContent from '@/content/alpha.json'
+import Head from 'next/head'
 
 export const Alpha = (): ReactElement => {
-  return <PageContent content={alphaContent} path="alpha.json" />
+  return (
+    <>
+      <PageContent content={alphaContent} path="alpha.json" />
+      <Head>
+        <meta name="robots" content="noindex" />
+        {/* Overwrite website favicons */}
+        <link rel="shortcut icon" href="/images/Alpha/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/Alpha/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/Alpha/favicons/favicon-16x16.png" />
+      </Head>
+    </>
+  )
 }
