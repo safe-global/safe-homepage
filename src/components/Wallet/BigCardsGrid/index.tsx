b5eeca0cc246552effbbd6c0d6650a6492ac9050
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-10-29 10:11:05 +0100

    fix: do not render the component without `items`

--- src/components/Wallet/BigCardsGrid/index.tsx
+++ src/components/Wallet/BigCardsGrid/index.tsx
@@ -3,22 +3,26 @@ import type { BaseBlock } from '@/components/Home/types'
 import layoutCss from '@/components/common/styles.module.css'
 import getComponentByName from '@/lib/getComponentByName'
 
-const BigCardsGrid = ({ items }: { items: Array<BaseBlock['items'] & { component: string }> }) => (
-  <Container className={layoutCss.containerShort}>
-    <Grid container spacing={{ xs: '30px', xl: '50px' }}>
-      {items?.map((item, index) => {
-        const { component } = item
+const BigCardsGrid = ({ items }: { items: Array<BaseBlock['items'] & { component: string }> }) => {
+  if (!items || !items.length) return null
 
-        const CardComponent = getComponentByName(`${component}`, () => <></>)
+  return (
+    <Container className={layoutCss.containerShort}>
+      <Grid container spacing={{ xs: '30px', xl: '50px' }}>
+        {items?.map((item, index) => {
+          const { component } = item
 
-        return (
-          <Grid key={index} item xs={12} md={6}>
-            <CardComponent {...item} />
-          </Grid>
-        )
-      })}
-    </Grid>
-  </Container>
-)
+          const CardComponent = getComponentByName(component, () => <></>)
+
+          return (
+            <Grid key={index} item xs={12} md={6}>
+              <CardComponent {...item} />
+            </Grid>
+          )
+        })}
+      </Grid>
+    </Container>
+  )
+}
 
 export default BigCardsGrid
