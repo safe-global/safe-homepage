a1b6ecb3b5b70d7ce2ecf81971a11e3493c11e60
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-10-29 11:03:13 +0100

    feat(Wallet): Big cards grid (#448)
    * feat: Hero section
    
    * fix: lint
    
    * feat: add "Watch demo" play button
    
    * fix: control video play
    
    * feat: add a bottom gradient over the paused video
    
    * styles: fix border-radius on video
    
    * feat: temporarily use /wallet-new slug because page path  conflct with the current version
    
    * feat: commonCMS/Marquee
    
    * feat: generic Card grid
    
    * feat: declare content in wallet.json
    
    * fix: undo unrelated change
    
    * fix: do not render the component without `items`
    
    * fix: remove conditional operator

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,19 +1,6 @@
-import type { InferGetStaticPropsType, NextPage } from 'next'
+import type { NextPage } from 'next'
 import { Wallet } from '@/components/Wallet'
-import { loadChainsData } from '@/lib/loadChainsData'
 
-const WalletPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (props) => {
-  return <Wallet {...props} />
-}
-
-export async function getStaticProps() {
-  const chainsData = await loadChainsData()
-
-  return {
-    props: {
-      chainsData,
-    },
-  }
-}
+const WalletPage: NextPage = () => <Wallet />
 
 export default WalletPage
