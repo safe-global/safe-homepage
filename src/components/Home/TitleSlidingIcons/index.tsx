53fa4e48da715bcd8ea4bf0e5f2073d3861465bb
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-10-29 10:59:29 +0100

    styles: cneter align section title

--- src/components/Home/TitleSlidingIcons/index.tsx
+++ src/components/Home/TitleSlidingIcons/index.tsx
@@ -1,21 +1,21 @@
 import { type BaseBlock } from '@/components/Home/types'
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 import IconCarouselElement, { type Carousel } from './IconCarouselElement'
 
-const TitleSlidingIcons = ({ title, text, icons, reverse = false }: BaseBlock & Carousel) => {
-  return (
-    <>
-      <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
-        <Typography variant="h3">{title}</Typography>
-        {text && (
-          <Typography color="primary.light" marginTop={4}>
-            {text}
-          </Typography>
-        )}
-      </Box>
-      <IconCarouselElement icons={icons} reverse={reverse} />
-    </>
-  )
-}
+const TitleSlidingIcons = ({ title, text, icons, reverse = false }: BaseBlock & Carousel) => (
+  <>
+    <Typography variant="h3" textAlign="center">
+      {title}
+    </Typography>
+
+    {text && (
+      <Typography color="primary.light" textAlign="center" marginTop={4}>
+        {text}
+      </Typography>
+    )}
+
+    <IconCarouselElement icons={icons} reverse={reverse} />
+  </>
+)
 
 export default TitleSlidingIcons
