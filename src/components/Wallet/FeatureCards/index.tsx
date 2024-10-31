6466a325a361f1cda16140efba57140d3eb7a4bf
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-10-31 12:19:06 +0100

    feat: ecosystem projects card

--- src/components/Wallet/FeatureCards/index.tsx
+++ src/components/Wallet/FeatureCards/index.tsx
@@ -1,8 +1,32 @@
 import type { BaseBlock } from '@/components/Home/types'
 import { Container, Grid, Typography } from '@mui/material'
 import layoutCss from '@/components/common/styles.module.css'
+import css from './styles.module.css'
+import LinkButton from '@/components/common/LinkButton'
+
+const CardContent = ({ caption, title, text, link }: Partial<BaseBlock>) => {
+  return (
+    <div className={css.cardContent}>
+      <Typography variant="caption" className={css.caption}>
+        {caption}
+      </Typography>
+
+      <Typography variant="h4">{title}</Typography>
+
+      <Typography color="primary.light">{text}</Typography>
+
+      {link ? (
+        <LinkButton underline={false} href={link.href}>
+          {link.title}
+        </LinkButton>
+      ) : undefined}
+    </div>
+  )
+}
 
 const FeatureCards = ({ title, text, items = [] }: BaseBlock) => {
+  const lastItem = items[items.length - 1]
+
   return (
     <Container className={layoutCss.containerMedium}>
       <Grid container justifyContent="space-between" columnSpacing="30px" rowGap="30px">
@@ -10,9 +34,20 @@ const FeatureCards = ({ title, text, items = [] }: BaseBlock) => {
           <Typography variant="h2">{title}</Typography>
         </Grid>
 
-        {text && (
-          <Grid item xs={12} md={5} alignContent="flex-end">
-            <Typography variant="h5">{text}</Typography>
+        <Grid item xs={12} md={5} alignContent="flex-end">
+          <Typography variant="h5">{text}</Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container mt={{ xs: '50px', md: '100px' }}>
+        {/* last item is part of the component */}
+        {lastItem && (
+          <Grid item xs={12}>
+            <div className={`${css.card} ${css.lastCard}`}>
+              <img className={css.image} src={lastItem.image?.src} alt={lastItem.image?.alt} />
+
+              <CardContent {...lastItem} />
+            </div>
           </Grid>
         )}
       </Grid>
