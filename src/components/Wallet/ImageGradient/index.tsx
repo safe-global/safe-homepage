0d7159808718584b9fdac574a8e775c8d6b4e4a5
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-11-06 16:34:30 +0100

    fix: copy changes

--- src/components/Wallet/ImageGradient/index.tsx
+++ src/components/Wallet/ImageGradient/index.tsx
@@ -2,12 +2,16 @@ import { Container, Divider } from '@mui/material'
 import type { BaseBlock } from '@/components/Home/types'
 import css from './styles.module.css'
 
-const ImageGradient = ({ image }: Partial<BaseBlock>) => (
-  <Container className={css.container}>
-    <img src={image?.src} alt={image?.alt} />
+const ImageGradient = ({ image }: Partial<BaseBlock>) => {
+  if (!image) return null
 
-    <Divider />
-  </Container>
-)
+  return (
+    <Container className={css.container}>
+      <img src={image.src} alt={image.alt} />
+
+      <Divider />
+    </Container>
+  )
+}
 
 export default ImageGradient
