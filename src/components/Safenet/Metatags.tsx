21a97a8b658b466cab194742c72051236b2a5be3
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-11-21 11:21:17 +0100

    feat: Metadata

---
+++ src/components/Safenet/Metatags.tsx
@@ -0,0 +1,10 @@
+import MetaTags from '@/components/common/MetaTags'
+
+const Metadata = () => {
+  const title = 'Safenet landing page'
+  const description = 'description'
+
+  return <MetaTags pageTitle={title} description={description} />
+}
+
+export default Metadata
--- src/components/Safenet/index.tsx
+++ src/components/Safenet/index.tsx
@@ -1,6 +1,9 @@
+import Metadata from '@/components/Safenet/Metatags'
+
 const Safenet = () => {
   return (
     <div>
+      <Metadata />
       <h1>Safenet</h1>
     </div>
   )
