4b5c0aebf036bd96f68b2e3c9b4f905656626d3b
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-11-26 11:25:16 +0100

    feat: add Roadmap tags

--- src/components/Safenet/Roadmap/index.tsx
+++ src/components/Safenet/Roadmap/index.tsx
@@ -1,20 +1,23 @@
 import { Button, Container, Grid, IconButton, Typography } from '@mui/material'
 import GradientChip from '@/components/Safenet/GradientChip'
+import { ALPHA_TELEGRAM_LINK } from '@/config/constants'
 import TelegramGradient from '@/public/images/Safenet/telegram-gradient.svg'
 import css from './styles.module.css'
-import { ALPHA_TELEGRAM_LINK } from '@/config/constants'
 
-const items: Array<{ title: string; features: Array<string> }> = [
+const items: Array<{ title: string; tag: string; features: Array<string> }> = [
   {
     title: 'Alpha',
+    tag: 'Q1 2025',
     features: ['Cross-chain Accounts', 'Execution guarantees', 'Validity proofs'],
   },
   {
     title: 'Beta',
+    tag: 'Q2 2025',
     features: ['Safe Apps Support', 'SAFE Validator network'],
   },
   {
     title: 'Version 1',
+    tag: 'Q3-Q4 2025',
     features: ['Third-party Processors', 'Liquidity Network', 'ERC-7579 Support'],
   },
 ]
@@ -27,7 +30,8 @@ const Roadmap = () => {
 
         <Typography className={css.title}>
           <img src="/images/Safenet/roadmap-hook.png" alt="Addornment" className={css.hook} />
-          Safenet is coming <span className={css.titleGradient}>2025</span>
+          Safenet
+          <br /> is coming <span className={css.titleGradient}>2025</span>
         </Typography>
 
         <Typography className={css.text}>Builder mode on.</Typography>
@@ -37,7 +41,9 @@ const Roadmap = () => {
 
       <Grid container className={css.gridContainer}>
         {items.map(({ title, features }, index) => (
-          <Grid item md={3} className={css.blockItem} key={index}>
+          <Grid item xs={6} md={3} className={css.blockItem} key={index}>
+            <div className={`${css.mobileTag} ${css[`mobileTag${index + 1}`]}`}>Q1 2025</div>
+
             <Typography className={css.blockTitle}>{title}</Typography>
 
             <div className={css.blockText}>
