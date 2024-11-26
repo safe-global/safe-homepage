4142786700f9bd9b6aa3fffb416f7886e17a5f28
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-11-26 22:50:39 +0100

    feat: Architecture section xs and sm viewport

--- src/components/Safenet/Architecture/index.tsx
+++ src/components/Safenet/Architecture/index.tsx
@@ -1,13 +1,30 @@
-import { Container, Grid, Typography } from '@mui/material'
+import { Button, Container, Grid, MobileStepper, Step, Stepper, Typography } from '@mui/material'
 import css from './styles.module.css'
 import GradientChip from '@/components/Safenet/GradientChip'
 import LinkButton from '@/components/common/LinkButton'
+import BackButton from '@/public/images/Safenet/Architecture/back-button.svg'
+import NextButton from '@/public/images/Safenet/Architecture/next-button.svg'
+import { useState } from 'react'
 
 const GradientStep = ({ stepNumber }: { stepNumber: number }) => {
   return <div className={css.stepCircle}>{stepNumber}</div>
 }
 
+const STEPS_NUMBER = 3
+const TRANSLATION_VALUES = [400, 0, -400]
+
 const Architecture = () => {
+  const [activeStep, setActiveStep] = useState(1)
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1)
+  }
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
+
+  console.log('activeStep', activeStep, (activeStep - 1) * 400)
   return (
     <div className={css.anchor}>
       <div className={css.arc} />
@@ -21,6 +38,108 @@ const Architecture = () => {
               <span className={css.titleGradient}>Reimagining</span> onchain transactions
             </>
           </Typography>
+        </div>
+
+        {/* Mobile Stepper */}
+        <div className={css.stepperWrapper}>
+          <Stepper activeStep={activeStep} style={{ transform: `translateX(${TRANSLATION_VALUES[activeStep]}px)` }}>
+            <Step>
+              <img src="/images/Safenet/Architecture/processor.png" alt="Safe processor" className={css.blockImage} />
+              <Typography className={css.blockTitle}>Processors</Typography>
+              <ul>
+                <li>Connect to user accounts and orchestrate optimal execution of transaction intents.</li>
+                <li>Provide execution guarantees like security checks and custom policies.</li>
+                <li>
+                  Execute transactions instantly by locking user assets (resource-lock) and providing liquidity
+                  optimistically.
+                </li>
+              </ul>
+            </Step>
+
+            <Step>
+              <Typography className={css.midItemTitle}>USER</Typography>
+
+              <Grid container className={css.midItemContainer}>
+                <Grid item md={12} className={css.step}>
+                  <div className={css.stepContent}>
+                    <GradientStep stepNumber={1} />
+                    <Typography className={css.stepText}>User signs transaction intent</Typography>
+                  </div>
+                </Grid>
+
+                <Grid item md={12} className={css.step}>
+                  <div className={css.stepContent}>
+                    <GradientStep stepNumber={2} />
+                    <Typography className={css.stepText}>Checks policies and issues resource-lock</Typography>
+                  </div>
+                </Grid>
+
+                <Grid item md={12} className={css.step}>
+                  <div className={css.stepContent}>
+                    <GradientStep stepNumber={3} />
+                    <Typography className={css.stepText}>Provides liquidity</Typography>
+                  </div>
+                </Grid>
+
+                <Grid item md={12} className={css.step}>
+                  <div className={css.stepContent}>
+                    <GradientStep stepNumber={4} />
+                    <Typography className={css.stepText}>Executes transaction intent</Typography>
+                  </div>
+                </Grid>
+
+                <Grid item md={12} className={css.step}>
+                  <div className={css.stepContent}>
+                    <GradientStep stepNumber={5} />
+                    <Typography className={css.stepText}>
+                      Debits user by providing a validity proof of executed intent
+                    </Typography>
+                  </div>
+                </Grid>
+
+                <Grid item md={12} className={css.step}>
+                  <div className={css.stepContent}>
+                    <GradientStep stepNumber={6} />
+                    <Typography className={css.stepText}>Returns liquidity</Typography>
+                  </div>
+                </Grid>
+              </Grid>
+            </Step>
+
+            <Step>
+              <img src="/images/Safenet/Architecture/mesh.png" alt="Safe net" className={css.blockImage} />
+              <Typography className={css.blockTitle}>Liquid Network</Typography>
+              <ul>
+                <li>Provides Safenet Processors with liquidity to execute transaction intents</li>
+                <li>Aggregates existing DeFi primitives</li>
+              </ul>
+
+              <div className={css.chipsContainer}>
+                <GradientChip caption="Bridges" />
+                <GradientChip caption="Clearing Layer" />
+                <GradientChip caption="Proprietary Liquidity (Solver)" />
+                <GradientChip caption="AMM" />
+                <GradientChip caption="Lending Markets" />
+              </div>
+            </Step>
+          </Stepper>
+          <MobileStepper
+            className={css.mobileStepper}
+            variant="progress"
+            steps={STEPS_NUMBER}
+            position="static"
+            activeStep={activeStep}
+            nextButton={
+              <Button size="small" onClick={handleNext} disabled={activeStep === STEPS_NUMBER - 1}>
+                <NextButton />
+              </Button>
+            }
+            backButton={
+              <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                <BackButton />
+              </Button>
+            }
+          />
         </div>
 
         <Grid container justifyContent="space-between" className={css.gridContainer}>
