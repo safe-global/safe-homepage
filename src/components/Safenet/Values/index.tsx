8e91892e0fd562646df84bb08db43c71b8324e56
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-11-27 13:48:00 +0100

    styles: Values section images

--- src/components/Safenet/Values/index.tsx
+++ src/components/Safenet/Values/index.tsx
@@ -23,6 +23,30 @@ const items: Array<BlockProps> = [
   },
 ]
 
+const stats = [
+  {
+    title: '$100B+',
+    text: 'Total Value Locked (TVL)',
+    link: {
+      href: 'https://dune.com/safe/all',
+    },
+  },
+  {
+    title: '6.4M',
+    text: 'Monthly Active Users (MAU)',
+    link: {
+      href: 'https://dune.com/queries/3670603',
+    },
+  },
+  {
+    title: '$150B+',
+    text: 'Total Volume Processed (TVP)',
+    link: {
+      href: 'https://dune.com/safe/all',
+    },
+  },
+]
+
 const BlockItem = ({ title, features }: BlockProps) => (
   <div className={css.blockItem}>
     <Typography className={css.blockTitle}>{title}</Typography>
@@ -49,6 +73,7 @@ const Values = () => (
 
       <div className={css.blocksStack}>
         <div className={css.stack} />
+        <img src="/images/Safenet/Values/processor.png" alt="Processor image" className={css.processor} />
 
         {items.map((item, index) => (
           <BlockItem {...item} key={index} />
@@ -57,15 +82,30 @@ const Values = () => (
 
       <div className={css.blocksWrapper}>
         <div className={css.blocks} />
+        <img src="/images/Safenet/Values/processor.png" alt="Processor image" className={css.processor} />
+        <img src="/images/Safenet/Values/cross.png" alt="Cross image" className={css.cross} />
 
         <Grid container className={css.blocksContainer}>
           {items.map((item, index) => (
-            <Grid item>
-              <BlockItem {...item} key={index} />
+            <Grid item key={index}>
+              <BlockItem {...item} />
             </Grid>
           ))}
         </Grid>
       </div>
+
+      <Grid container spacing={{ xs: '30px', xl: '50px' }} justifyContent="space-between">
+        {stats?.map((item, index) => (
+          <Grid item xs={12} md={4} key={index}>
+            <a href="" target="_blank" rel="noreferrer" className={css.metric}>
+              {/* TODO: Fetch these values from a Dune query */}
+              <p className={css.value}>{item.title}</p>
+
+              <Typography variant="caption">{item.text}</Typography>
+            </a>
+          </Grid>
+        ))}
+      </Grid>
 
       <div className={css.buttonWrapper}>
         <LinkButton>Safenet Vision</LinkButton>
