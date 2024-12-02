610b2160920899808c318c0d45cd4d56e56e4f24
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-12-02 15:46:11 +0100

    fix: typo

--- src/components/Safenet/CrossChain/index.tsx
+++ src/components/Safenet/CrossChain/index.tsx
@@ -25,7 +25,7 @@ const items: BaseBlock['items'] = [
   {
     image: {
       src: '/images/Safenet/CrossChain/padlock.png',
-      alt: 'Verifiyed padlock',
+      alt: 'Verified padlock',
     },
     title: (
       <>
