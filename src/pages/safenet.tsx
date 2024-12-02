e9e684620ed5da3509520005bc2e11bcaec650bb
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-12-02 14:08:31 +0100

    chore: favicons

--- src/pages/safenet.tsx
+++ src/pages/safenet.tsx
@@ -2,9 +2,20 @@ import type { ReactElement } from 'react'
 import type { NextPageWithLayout } from '@/pages/_app'
 import Safenet from '@/components/Safenet'
 import SafenetLayout from '@/components/Safenet/Layout'
+import Head from 'next/head'
 
 const SafenetPage: NextPageWithLayout = () => {
-  return <Safenet />
+  return (
+    <>
+      <Safenet />
+      <Head>
+        {/* Favicons */}
+        <link rel="shortcut icon" href="/images/Safenet/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/Safenet/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/Safenet/favicons/favicon-16x16.png" />
+      </Head>
+    </>
+  )
 }
 
 SafenetPage.getLayout = function getLayout(page: ReactElement) {
