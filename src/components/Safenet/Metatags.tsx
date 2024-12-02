7bdcbe07b23efc709967f912b7bd5778648dab0f
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2024-12-02 13:09:09 +0100

    chore: add meta image

--- src/components/Safenet/Metatags.tsx
+++ src/components/Safenet/Metatags.tsx
@@ -4,7 +4,7 @@ const Metadata = () => {
   const title = 'INTRODUCING SAFENET - NEW CHAIN ABSTRACTION PROTOCOL BY SAFE'
   const description = 'Instant Cross-chain Transactions. Execution Guarantees. On any network.'
 
-  return <MetaTags pageTitle={title} description={description} />
+  return <MetaTags pageTitle={title} description={description} image="/images/Safenet/meta-image-safenet.jpg" />
 }
 
 export default Metadata
