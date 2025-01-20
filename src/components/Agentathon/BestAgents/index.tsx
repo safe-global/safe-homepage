2ef42eb657222348495becc97d97c1245b6f933c
Rohan Sharma <rohansharma5494@gmail.com>
2025-01-20 21:48:16 +0530

    feat: 🎸 added best agents section

---
+++ src/components/Agentathon/BestAgents/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import type { BaseBlock } from '@/components/Home/types'
+import { Typography } from '@mui/material'
+import css from './style.module.css'
+import ButtonsWrapper from '@/components/common/ButtonsWrapper'
+
+export default function BestAgents({ caption, items, buttons }: BaseBlock) {
+  return (
+    <section className={css.sectionContainer}>
+      <Typography variant="caption" className={css.caption} pb={5}>
+        {caption}
+      </Typography>
+      <div className={css.itemsWrapper}>
+        {items &&
+          items.map((item, index) => (
+            <div className={css.itemWrapper} key={index}>
+              <img src={item.image?.src} className={css.itemImage} alt={item.image?.alt} />
+              <Typography variant="body1" className={css.itemTitle}>
+                {item.title}
+              </Typography>
+            </div>
+          ))}
+      </div>
+      <ButtonsWrapper buttons={buttons} />
+    </section>
+  )
+}
