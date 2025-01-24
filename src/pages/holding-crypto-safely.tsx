26ea584d75fab38d5b75359c9b2cd2ca34fbd573
OhColar <alex@arcdevelop.co.uk>
2025-01-24 14:30:34 +0000

    linkbuilding page added: holding-crypto-safely

---
+++ src/pages/holding-crypto-safely.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import {HoldingCryptoSafely} from '@/components/HoldingCryptoSafely'
+
+const HoldingCryptoSafelyPage: NextPage = () => {
+  return <HoldingCryptoSafely />
+}
+
+export default HoldingCryptoSafelyPage
