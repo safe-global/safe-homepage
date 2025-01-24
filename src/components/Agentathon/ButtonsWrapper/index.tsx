03eead4315ba4ce9aa8dca633f52dcedcabc0275
Rohan Sharma <rohansharma5494@gmail.com>
2025-01-24 18:33:47 +0530

    fix: 🐛 ui bugs for hero section'

--- src/components/Agentathon/ButtonsWrapper/index.tsx
+++ src/components/Agentathon/ButtonsWrapper/index.tsx
@@ -2,6 +2,7 @@ import { Button } from '@mui/material'
 import css from './style.module.css'
 import SafeLink from '@/components/common/SafeLink'
 import type { Button as ButtonType } from '@/components/Home/types'
+import ArrowIcon from '@/public/images/arrow-out-square-corner.svg'
 
 type ButtonsWrapperProps = {
   buttons?: ButtonType[]
@@ -20,15 +21,10 @@ const ButtonsWrapper = ({ buttons, mobileDirection }: ButtonsWrapperProps) => {
 
         return (
           <SafeLink key={index} href={href} isDisabled={!!button.isDisabled}>
-            {isButton ? (
-              <Button variant="contained" size="large" color={color}>
-                {text}
-              </Button>
-            ) : (
-              <Button variant="outlined" size="large" color={color}>
-                {text}
-              </Button>
-            )}
+            <Button variant={isButton ? 'contained' : 'outlined'} size="large" color={color} className={css.button}>
+              {text}
+              {isButton && <ArrowIcon className={css.icon} />}
+            </Button>
           </SafeLink>
         )
       })}
