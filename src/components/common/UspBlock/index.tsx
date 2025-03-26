c7dfa8f3da1f007bfef84616c764d017a419c205
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2025-03-26 11:01:01 +0100

    refactor: common/UspBlock revert variant conditionals

--- src/components/common/UspBlock/index.tsx
+++ src/components/common/UspBlock/index.tsx
@@ -9,58 +9,39 @@ import LinkButton from '@/components/common/LinkButton'
 
 export const GridItem = ({
   image,
+  caption,
   title,
   text,
   link,
   width = 4,
-  containerClass,
-}: Partial<BaseBlock> & { width: GridProps['md']; containerClass?: string }): ReactElement => {
+}: Partial<BaseBlock> & { width: GridProps['md'] }): ReactElement => {
   return (
     <Grid item xs={12} md={width} className={css.gridItems}>
       <div>
         {image ? <img {...image} /> : null}
 
-        {containerClass === 'chapter-cont' ? (
-          <>
-            <Typography color="primary.light" mt={2} className={css.chapterTitle}>
-              {text}
-            </Typography>
-            <Typography variant="h5" className={css.title}>
-              {title}
-            </Typography>
-          </>
-        ) : (
-          <>
-            <Typography variant="h5" className={css.title}>
-              {title}
-            </Typography>
-            <Typography color="primary.light">{text}</Typography>
-          </>
-        )}
+        <Typography variant="caption" mt={2} color="text.primary" component="div">
+          {caption}
+        </Typography>
+
+        <Typography variant="h5" className={css.title}>
+          {title}
+        </Typography>
+
+        <Typography color="primary.light">{text}</Typography>
       </div>
 
-      {link &&
-        (containerClass === 'chapter-cont' ? (
-          <Link href={link.href}>
-            <LinkButton>{link.title}</LinkButton>
-          </Link>
-        ) : (
-          <Link href={link.href} target="_blank" rel="noreferrer" passHref>
-            <LinkButton>{link.title}</LinkButton>
-          </Link>
-        ))}
+      {link && (
+        <Link href={link.href}>
+          <LinkButton>{link.title}</LinkButton>
+        </Link>
+      )}
     </Grid>
   )
 }
 
 // TODO: unify the ItemGrid and accept a GridItem component
-const UspBlock = ({
-  variant,
-  title,
-  text,
-  items,
-  containerClass,
-}: BaseBlock & { variant: '3-columns' | '4-columns'; containerClass: string }) => (
+const UspBlock = ({ variant, title, text, items }: BaseBlock & { variant: '3-columns' | '4-columns' }) => (
   <Container>
     <Grid container className={layoutCss.containerShort} flexDirection="column" alignItems="center">
       <Typography variant="h2" mb={3} textAlign={{ md: 'center' }}>
@@ -71,7 +52,7 @@ const UspBlock = ({
 
       <Grid container className={css.roundCorners} mt={{ xs: 3, md: 7 }}>
         {items?.map((item, index) => (
-          <GridItem key={index} width={variant === '3-columns' ? 4 : 3} containerClass={containerClass} {...item} />
+          <GridItem key={index} width={variant === '3-columns' ? 4 : 3} {...item} />
         ))}
       </Grid>
     </Grid>
